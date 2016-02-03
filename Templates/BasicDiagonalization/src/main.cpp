067310c3db90d8064d3a137e2bbcb230f0164a35
Kristofer Bjornson <dafer45@hotmail.com>
2016-02-03 01:05:35 +0100

    Created Template/BasicDiagonalization/

---
+++ Templates/BasicDiagonalization/src/main.cpp
@@ -0,0 +1,63 @@
+/* Basic example of diagonalization of a 2D tight-binding model with t = 1 and
+ * mu = -1. Lattice with edges and a size of 20x20 sites.
+ */
+
+#include <iostream>
+#include <complex>
+#include "AmplitudeSet.h"
+#include "Model.h"
+#include "FileWriter.h"
+#include "PropertyExtractor.h"
+
+using namespace std;
+
+const complex<double> i(0, 1);
+
+int main(int argc, char **argv){
+	//Lattice size
+	const int SIZE_X = 20;
+	const int SIZE_Y = 20;
+
+	//Parameters
+	complex<double> mu = -1.0;
+	complex<double> t = 1.0;
+
+	//Create model and set up hopping parameters
+	Model model;
+	for(int x = 0; x < SIZE_X; x++){
+		for(int y = 0; y < SIZE_Y; y++){
+			for(int s = 0; s < 2; s++){
+				//Add hopping ampltudes corresponding to chemical potential
+				model.addHA(HoppingAmplitude({x, y, s},		{x, y, s},	-mu));
+				model.addHA(HoppingAmplitude({x, y, s+2},	{x, y, s+2},	mu));
+
+				//Add hopping parameters corresponding to t
+				if(x+1 < SIZE_X){
+					model.addHAAndHC(HoppingAmplitude({x, y, s},	{(x+1)%SIZE_X, y, s},		-t));
+					model.addHAAndHC(HoppingAmplitude({x, y, s+2},	{(x+1)%SIZE_X, y, s+2},		t));
+				}
+				if(y+1 < SIZE_Y){
+					model.addHAAndHC(HoppingAmplitude({x, y, s},	{x, (y+1)%SIZE_Y, s},	-t));
+					model.addHAAndHC(HoppingAmplitude({x, y, s+2},	{x, (y+1)%SIZE_Y, s+2},	t));
+				}
+			}
+		}
+	}
+
+	//Construct and run system
+	model.construct();
+	model.run();
+
+	//Remove any TBResults.h5 file already in the folder
+	FileWriter::clear();
+
+	//Create property extractor
+	PropertyExtractor pe(&model);
+
+	//Extract eigenvalues and write these to file
+	double *ev = pe.getEV();
+	FileWriter::writeEV(ev, model.getBasisSize());
+	delete [] ev;
+
+	return 0;
+}
--- init.sh
+++ init.sh
@@ -2,4 +2,10 @@ module load gcc/4.9
 module load cuda/7.0
 
 LD_LIBRARY_PATH+=":$PWD/hdf5/hdf5-build/hdf5/lib"
+LD_LIBRARY_PATH+=":$PWD/TBTK/calc/TightBindingLib/build"
 
+LIBRARY_PATH+="$PWD/TBTK/calc/TightBindingLib/build"
+export LIBRARY_PATH
+
+CPLUS_INCLUDE_PATH="/$PWD/TBTK/calc/TightBindingLib/include"
+export CPLUS_INCLUDE_PATH
