46b510d9f129ed9acdacfc1454e750a812fa82c7
Kristofer Björnson <dafer45@hotmail.com>
2016-02-13 00:24:27 +0100

    Updated init_session.sh to correctly set LD_LIBRARY_PATH also when it is not already set

--- init_session.sh
+++ init_session.sh
@@ -4,5 +4,10 @@ module load cuda/7.0
 TBTK_dir=$PWD
 export TBTK_dir
 
-LD_LIBRARY_PATH+=:${TBTK_dir}/hdf5/hdf5-build/hdf5/lib
+if [ -z "$LD_LIBRARY_PATH" ]
+then
+	LD_LIBRARY_PATH=${TBTK_dir}/hdf5/hdf5-build/hdf5/lib;
+else
+	LD_LIBRARY_PATH+=:${TBTK_dir}/hdf5/hdf5-build/hdf5/lib;
+fi
 export LD_LIBRARY_PATH
