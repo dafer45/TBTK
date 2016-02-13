1da214a6ffe4d1e67f9286ad2cc4c1459619f28c
Kristofer Björnson <dafer45@hotmail.com>
2016-02-13 12:02:08 +0100

    Updated plotDOS.py and added Templates/BasixDiagonalization/plot.sh

--- TBTK/view/python/plotDOS.py
+++ TBTK/view/python/plotDOS.py
@@ -6,11 +6,12 @@ import scipy.ndimage.filters
 import pylab
 import sys
 
-if(len(sys.argv) != 2):
-	print "Error: Needs one argument for .hdf5-filename"
+if(len(sys.argv) != 3):
+	print "Error, the following arguments are needed: .hdf5-file, sigma"
 	exit(1)
 
 filename = sys.argv[1]
+sigma = float(sys.argv[2])
 
 file = h5py.File(filename, 'r');
 dataset = file['DOS']
@@ -20,12 +21,12 @@ resolution = dataset.size
 print "UpLowLimits: " + str(dataset.attrs['UpLowLimits'])
 print "Resolution: " + str(resolution)
 
-energies = numpy.linspace(limits[0], limits[1], resolution)
-
-sigma = 3
+energies = numpy.linspace(limits[1], limits[0], resolution)
+sigma_discrete_units = sigma*resolution/(limits[0] - limits[1])
 
 fig = matplotlib.pyplot.figure()
-matplotlib.pyplot.plot(energies, scipy.ndimage.filters.gaussian_filter1d(dataset, sigma))
-#matplotlib.pyplot.show()
+matplotlib.pyplot.plot(energies, scipy.ndimage.filters.gaussian_filter1d(dataset, sigma_discrete_units))
+matplotlib.pyplot.xlabel('E');
+matplotlib.pyplot.ylabel('DOS');
 
 fig.savefig('figures/DOS.png')
---
+++ Templates/BasicDiagonalization/plot.sh
@@ -0,0 +1,1 @@
+python ${TBTK_dir}/TBTK/view/python/plotDOS.py TBTKResults.h5 0.15
