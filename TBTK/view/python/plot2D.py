4a7ccacce47bee538e7f04e19c8a508df3215f46
Kristofer Björnson <dafer45@hotmail.com>
2016-02-13 20:05:38 +0100

    Updated plot2d.py and added plot.sh to two templates

--- TBTK/view/python/plot2D.py
+++ TBTK/view/python/plot2D.py
@@ -7,17 +7,17 @@ import scipy.ndimage.filters
 import mpl_toolkits.mplot3d
 import sys
 
-if(len(sys.argv) != 2):
-	print "Error: Needs one argument for .hdf5-filename"
+if(len(sys.argv) != 3):
+	print "Error, the following arguments are needed: .hdf5-file, dataset name"
 	exit(1)
 
 filename = sys.argv[1]
+dataset_name = sys.argv[2]
 
 file = h5py.File(filename, 'r');
-dataset_abs = file['Dabs']
-dataset_arg = file['Darg']
+dataset = file[dataset_name]
 
-dimensions = dataset_abs.shape
+dimensions = dataset.shape
 print "Dimensions: " + str(dimensions)
 if(len(dimensions) != 2):
 	print "Error, can only plot for 2 physical dimensions"
@@ -25,21 +25,14 @@ if(len(dimensions) != 2):
 
 x = numpy.arange(0, dimensions[0], 1)
 y = numpy.arange(0, dimensions[1], 1)
-X, Y = numpy.meshgrid(x, y)
+X, Y = numpy.meshgrid(y, x)
 
 fig = matplotlib.pyplot.figure()
 ax = fig.gca(projection='3d')
 
-Z = dataset_abs[:,:]
+Z = dataset[:,:]
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=matplotlib.cm.coolwarm, linewidth=0, antialiased=False)
 ax.set_zlim(numpy.min(Z), numpy.max(Z))
-#matplotlib.pyplot.show()
 
-fig.savefig('figures/Dabs.png')
+fig.savefig('figures/' + dataset_name + '.png')
 
-Z = dataset_arg[:,:]
-fig = matplotlib.pyplot.figure()
-ax = fig.gca()
-ax.pcolormesh(X, Y, Z, cmap=matplotlib.cm.coolwarm)
-
-fig.savefig('figures/Darg.png')
