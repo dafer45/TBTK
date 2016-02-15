0485270de252d9ca4e52be4216ebfe9ce380b483
Kristofer Bjornson <dafer45@hotmail.com>
2016-02-15 20:44:32 +0100

    Added doxygen comments to AmplitudeSet.h

--- TBTK/calc/TightBindingLib/include/AmplitudeSet.h
+++ TBTK/calc/TightBindingLib/include/AmplitudeSet.h
@@ -13,35 +13,67 @@
 #include "TreeNode.h"
 #include <iostream>
 
+/** An AmplitudeSet is a container for HoppingAmplitudes.
+ *  The structure contains the root node for the tree structure in which the
+ *  HoppingAmplitudes are stored, as well as functions for adding and accessing
+ *  HoppingAmplitudes. Once all HoppingAmplitudes have been added to the
+ *  AmplitudeSet, the construct method has to be called in order to construct
+ *  an appropriate Hilbert space. The AmplitudeSet is most importantly used by
+ *  the Model to store the Hamiltonian.
+ */
 class AmplitudeSet{
 public:
+	/** Root node for the tree structure in which HoppingAmplitudes are
+	 *  stored*/
 	TreeNode tree;
 
+	/** Add a single HoppingAmplitude. */
 	void addHA(HoppingAmplitude ha);
+	/** Add a HoppingAmplitude and its Hermitian conjugate. */
 	void addHAAndHC(HoppingAmplitude ha);
+
+	/** Get all HoppingAmplitudes with given 'from'-index.
+	 *  @param index 'From'-index to get HoppingAmplitudes for. */
 	std::vector<HoppingAmplitude>* getHAs(Index index);
+	/** Get Hilbert space index corresponding to given 'from'-index.
+	 *  @param index 'From'-index to get Hilbert space index for. */
 	int getBasisIndex(const Index &index);
+	/** Get size of Hilbert space. */
 	int getBasisSize();
 
+	/** Construct Hilbert space. No more HoppingAmplitudes should be added
+	 *  after this call. */
 	void construct();
 
+	/** Iterator for iterating through HoppingAmplitudes. */
 	class iterator{
 	public:
+		/** Destructor. */
 		~iterator();
 
+		/** Reset iterator. */
 		void reset();
+		/** Iterate to next HoppingAmplitude. */
 		void searchNextHA();
+		/** Get current HoppingAmplitude. */
 		HoppingAmplitude* getHA();
 	private:
+		/** The iterator can only be constructed by the AmplitudeSet. */
 		friend class AmplitudeSet;
+		/** Private constructor. Limits the ability to construct the
+		 *  iterator to the AmplitudeSet. */
 		iterator(TreeNode *tree);
+		/** TreeNode iterator. Implements the actual iteration. */
 		TreeNode::iterator* it;
 	};
 
+	/** Returns an iterator for iterating through HoppingAmplitudes. */
 	AmplitudeSet::iterator getIterator();
 
+	/** Print tree structure. Mainly for debuging. */
 	void print();
 
+	/** Tabulates HoppingAmplitudes to make them possible to export. */
 	void tabulate(int **table, int *dims);
 private:
 };
