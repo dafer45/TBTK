065e74181d0bc5b195adf12cdfb879071a92db4c
Kristofer Björnson <dafer45@hotmail.com>
2016-02-16 22:51:37 +0100

    Updated plotLDOS.py and modified plot script in Templates/WireOnSuperconductor to use it

--- TBTK/view/python/plotLDOS.py
+++ TBTK/view/python/plotLDOS.py
@@ -7,17 +7,18 @@ import scipy.ndimage.filters
 import mpl_toolkits.mplot3d
 import sys
 
-if(len(sys.argv) != 2):
-	print "Error: Needs one argument for .hdf5-filename"
+if(len(sys.argv) != 3):
+	print "Error, the following parameters are needed: .hdf5-file, sigma"
 	exit(1)
 
 filename = sys.argv[1]
+sigma = float(sys.argv[2])
 
 file = h5py.File(filename, 'r');
-dataset = file['SP_LDOS_E']
+dataset = file['SP_LDOS']
 
 data_dimensions = dataset.shape
-physical_dimensions = len(data_dimensions) - 2 #Two last dimensions are for energy and spin components.
+physical_dimensions = len(data_dimensions) - 3 #Three last dimensions are for energy, spin components, and real/imaginary decomposition.
 energy_resolution = data_dimensions[physical_dimensions];
 limits = dataset.attrs['UpLowLimits']
 print "Dimensions: " + str(physical_dimensions)
@@ -27,24 +28,22 @@ if(physical_dimensions != 1):
 	print "Error, can only plot for 1 physical dimensions"
 	exit(0)
 
+size_x = data_dimensions[0]
+size_y = data_dimensions[1]
+
 x = numpy.arange(0, data_dimensions[0], 1)
 y = numpy.arange(limits[1], limits[0], (limits[0] - limits[1])/energy_resolution)
-X, Y = numpy.meshgrid(y, x)
+X, Y = numpy.meshgrid(x, y)
 
-for n in range(0, 5):
-	fig = matplotlib.pyplot.figure()
-	Z = dataset[:,:,n]
+fig = matplotlib.pyplot.figure()
+Z = dataset[:,:,0,0] + dataset[:,:,3,0]
+sigma_discrete_units = sigma*energy_resolution/(limits[0] - limits[1])
+for xp in range(0, size_x):
+	Z[xp,:] = scipy.ndimage.filters.gaussian_filter1d(Z[xp,:], sigma_discrete_units)
 
-	#3D figure
-#	ax = fig.gca(projection='3d')
-#	ax.view_init(90, 0)
-#	ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=matplotlib.cm.coolwarm, linewidth=0, antialiased=False)
-#	ax.set_zlim(numpy.min(Z), numpy.max(Z))
+#Color map figure
+ax = fig.gca()
+ax.pcolormesh(X.transpose(), Y.transpose(), Z, cmap=matplotlib.cm.coolwarm)
 
-	#Color map figure
-	ax = fig.gca()
-	ax.pcolormesh(Y, X, Z, cmap=matplotlib.cm.coolwarm)
-
-#	matplotlib.pyplot.show()
-	fig.savefig('figures/SP_LDOS_E' + str(n) + '.png')
+fig.savefig('figures/LDOS.png')
 
--- Templates/WireOnSuperconductor/plot.sh
+++ Templates/WireOnSuperconductor/plot.sh
@@ -3,4 +3,9 @@
 #Plot D_abs and D_arg in TBTKResults.h5
 python ${TBTK_dir}/TBTK/view/python/plot2D.py TBTKResults.h5 D_abs
 python ${TBTK_dir}/TBTK/view/python/plot2D.py TBTKResults.h5 D_arg
+
+#Plot magnetization
 python ${TBTK_dir}/TBTK/view/python/plotMAG.py TBTKResults.h5
+
+#Plot local density of states using Gaussian smoothing with sigma = 0.02
+python ${TBTK_dir}/TBTK/view/python/plotLDOS.py TBTKResults.h5 0.02
