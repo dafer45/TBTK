4dd58ab7f90f6afe07e36f9e243eae9356a06d2f
Kristofer Björnson <dafer45@hotmail.com>
2016-02-16 22:20:29 +0100

    Updated plotscript in Templates/WireOnSuperconductor

--- Templates/WireOnSuperconductor/plot.sh
+++ Templates/WireOnSuperconductor/plot.sh
@@ -3,3 +3,4 @@
 #Plot D_abs and D_arg in TBTKResults.h5
 python ${TBTK_dir}/TBTK/view/python/plot2D.py TBTKResults.h5 D_abs
 python ${TBTK_dir}/TBTK/view/python/plot2D.py TBTKResults.h5 D_arg
+python ${TBTK_dir}/TBTK/view/python/plotMAG.py TBTKResults.h5
