b3b7838ecf7d2997a561be4e7d60985e8dca6e83
Kristofer Björnson <dafer45@hotmail.com>
2016-03-12 22:58:18 +0100

    Commitng staged file to allow the to subsequently be removed

--- doc/html/search/namespaces_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['plot1d',['plot1D',['../namespaceplot1D.html',1,'']]],
-  ['plot2d',['plot2D',['../namespaceplot2D.html',1,'']]],
-  ['plotdos',['plotDOS',['../namespaceplotDOS.html',1,'']]],
-  ['plotev',['plotEV',['../namespaceplotEV.html',1,'']]]
-];
