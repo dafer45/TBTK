aa0e96c638b50681334995d72627d1c4c3f3dbdd
Kristofer Björnson <dafer45@hotmail.com>
2016-08-24 19:19:09 +0200

    Implemented the Property SpinPolarizedLdos

--- TBTK/calc/TightBindingLib/src/SpinPolarizedLdos.cpp
+++ TBTK/calc/TightBindingLib/src/SpinPolarizedLdos.cpp
@@ -5,10 +5,34 @@
 
 #include "../include/SpinPolarizedLdos.h"
 
+using namespace std;
+
 namespace TBTK{
 namespace Property{
 
+SpinPolarizedLdos::SpinPolarizedLdos(int rank, const int *dims, double lowerLimit, double upperLimit, int resolution){
+	this->rank = rank;
+	this->dims = new int[rank];
+	for(int n = 0; n < rank; n++)
+		this->dims[n] = dims[n];
 
+	this->lowerLimit = lowerLimit;
+	this->upperLimit = upperLimit;
+	this->resolution = resolution;
+
+	size = 4*resolution;
+	for(int n = 0; n < rank; n++)
+		size *= dims[n];
+
+	data = new complex<double>[size];
+	for(int n = 0; n < size; n++)
+		data[n] = 0.;
+}
+
+SpinPolarizedLdos::~SpinPolarizedLdos(){
+	delete [] dims;
+	delete [] data;
+}
 
 };	//End of namespace Property
 };	//End of namespace TBTK
