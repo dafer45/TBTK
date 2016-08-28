c5ecfbe711e631a49e463df402710909ec825998
Kristofer Bjornson <dafer45@hotmail.com>
2016-08-29 01:03:45 +0200

    Added functions get-functions to Geometry that takes Hilbert space indices

--- TBTK/calc/TightBindingLib/include/Geometry.h
+++ TBTK/calc/TightBindingLib/include/Geometry.h
@@ -30,15 +30,25 @@ public:
 	/** Get number of specifiers. */
 	int getNumSpecifiers();
 
-	/** Get Coordinates. */
+	/** Get Coordinates using a physical index. */
 	const double* getCoordinates(const Index &index);
 
-	/** Get specifier. */
+	/** Get coordinates using a Hilbert space index. */
+	const double* getCoordinates(int index);
+
+	/** Get specifier using a physical index. */
 	int getSpecifier(const Index &index, int specifier);
 
-	/** Get distance between positions corresponding to index1 and index2.
-	 */
+	/** Get specifier using a Hilbert space index. */
+	int getSpecifier(int index, int specifier);
+
+	/** Get distance between positions corresponding to index1 and index2,
+	 *  using physical indices. */
 	double getDistance(const Index &index1, const Index &index2);
+
+	/** Get distance between positions corresponding to index1 and index2,
+	 *  using physical indices. */
+	double getDistance(int index1, int index2);
 private:
 	/** Number of spatial dimensions. */
 	int dimensions;
@@ -68,8 +78,26 @@ inline const double* Geometry::getCoordinates(const Index &index){
 	return &(coordinates[dimensions*parentModel->getBasisIndex(index)]);
 }
 
+inline const double* Geometry::getCoordinates(int index){
+	return &(coordinates[dimensions*index]);
+}
+
 inline int Geometry::getSpecifier(const Index &index, int specifier){
 	return specifiers[numSpecifiers*parentModel->getBasisIndex(index) + specifier];
+}
+
+inline int Geometry::getSpecifier(int index, int specifier){
+	return specifiers[numSpecifiers*index + specifier];
+}
+
+inline double Geometry::getDistance(int index1, int index2){
+	double distanceSquared = 0.;
+	for(int n = 0; n < dimensions; n++){
+		double difference = coordinates[index1] - coordinates[index2];
+		distanceSquared += difference*difference;
+	}
+
+	return sqrt(distanceSquared);
 }
 
 };	//End of namespace TBTK
