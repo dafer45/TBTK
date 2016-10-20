991e886d1af7014683dee7c71651be297e0ddcee
glaurung24 <andreas.theiler@gmx.at>
2016-10-20 15:47:22 +0200

    (at) Added the types of string and bool to the ParameterSet

--- TBTK/calc/TightBindingLib/include/ParameterSet.h
+++ TBTK/calc/TightBindingLib/include/ParameterSet.h
@@ -3,6 +3,7 @@
  *  @brief Set of parameters
  *
  *  @author Kristofer Björnson
+ *  @author Andreas Theiler
  */
 
 #ifndef COM_DAFER45_TBTK_PARAMETER_SET
@@ -33,50 +34,86 @@ public:
 	/** Add complex parameter. */
 	void addComplex(std::string name, std::complex<double> value);
 
+	/** Add string parameter. */
+	void addString(std::string name, std::string value);
+
+	/** Add boolean parameter. */
+	void addBool(std::string name, bool value);
+
 	/** Get integer parameter. */
-	int getInt(std::string name);
+	int getInt(std::string name) const;
 
 	/** Get double parameter. */
-	double getDouble(std::string name);
+	double getDouble(std::string name) const;
 
 	/** Get complex parameter. */
-	std::complex<double> getComplex(std::string name);
+	std::complex<double> getComplex(std::string name) const;
+
+	/** Get string parameter. */
+	int getString(std::string name) const;
+
+    /** Get boolean parameter. */
+	int getBool(std::string name) const;
 
 	/** Get number of integer parameters. */
-	int getNumInt();
+	int getNumInt() const;
 
 	/** Get number of double parameters. */
-	int getNumDouble();
+	int getNumDouble() const;
 
 	/** Get number of complex parameters. */
-	int getNumComplex();
+	int getNumComplex() const;
+
+	/** Get number of string parameters. */
+	int getNumString() const;
+
+	/** Get number of boolean parameters. */
+	int getNumBool() const;
 
 	/** Get integer name. */
-	std::string getIntName(int n);
+	std::string getIntName(int n) const;
 
 	/** Get double name. */
-	std::string getDoubleName(int n);
+	std::string getDoubleName(int n) const;
 
 	/** Get complex name. */
-	std::string getComplexName(int n);
+	std::string getComplexName(int n) const;
+
+	/** Get string name. */
+	std::string getStringName(int n) const;
+
+	/** Get booleanname. */
+	std::string getBoolName(int n) const;
 
 	/** Get integer value. */
-	int getIntValue(int n);
+	int getIntValue(int n) const;
 
 	/** Get double value. */
-	double getDoubleValue(int n);
+	double getDoubleValue(int n) const;
 
 	/** Get complex value. */
-	std::complex<double> getComplexValue(int n);
+	std::complex<double> getComplexValue(int n) const;
+
+	/** Get string value. */
+	std::string getStringValue(int n) const;
+
+	/** Get boolean value. */
+	bool getBoolValue(int n) const;
 
 	/** Returns true if an integer parameter with given name exists. */
-	bool intExists(std::string name);
+	bool intExists(std::string name) const;
 
 	/** Returns true if a double parameter with given name exists. */
-	bool doubleExists(std::string name);
+	bool doubleExists(std::string name) const;
 
 	/** Returns true if a complex parameter with given name exists. */
-	bool complexExists(std::string name);
+	bool complexExists(std::string name) const;
+
+    /** Returns true if an string parameter with given name exists. */
+	bool stringExists(std::string name) const;
+
+    /** Returns true if an boolean parameter with given name exists. */
+	bool boolExists(std::string name) const;
 private:
 	/** Integer parameters. */
 	std::vector<std::tuple<std::string, int>> intParams;
@@ -86,6 +123,12 @@ private:
 
 	/** Complex parameters. */
 	std::vector<std::tuple<std::string, std::complex<double>>> complexParams;
+
+    /** String parameters. */
+	std::vector<std::tuple<std::string, std::string>> stringParams;
+
+    /** Boolean parameters. */
+	std::vector<std::tuple<std::string, bool>> boolParams;
 };
 
 };	//End of namespace Util
