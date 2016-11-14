28e2aaa0732b7114600693f93acc80d2a3185b39
Kristofer Björnson <dafer45@hotmail.com>
2016-11-14 01:29:41 +0100

    Added WignerSeitzCell

---
+++ Lib/include/Uncategorized/WignerSeitzCell.h
@@ -0,0 +1,67 @@
+/* Copyright 2016 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @package TBTKcalc
+ *  @file WignerSeitzCell.h
+ *  @brief Wigner-Seitz cell.
+ *
+ *  @author Kristofer Björnson
+ */
+
+#ifndef COM_DAFER45_TBTK_WIGNER_SEITZ_CELL
+#define COM_DAFER45_TBTK_WIGNER_SEITZ_CELL
+
+#include "Vector3d.h"
+
+#include <vector>
+#include <initializer_list>
+
+namespace TBTK{
+
+/** Wigner-Seitz cell. */
+class WignerSeitzCell{
+public:
+	/** Constructor. */
+	WignerSeitzCell(const std::vector<std::vector<double>> &basisVectors);
+
+	/** Destructor. */
+	~WignerSeitzCell();
+
+	/** Returns the index of the Wigner-Seitz cell corresponding to the
+	 *  given coordinate. */
+	std::vector<double> getCellIndex(
+		std::initializer_list<double> coordinate
+	) const;
+private:
+	/** Lattice dimension. */
+	unsigned dimensions;
+
+	/** Basis vectors stored as three Vector3d. For lower dimensions the
+	 *  vectors are padded with zeros to create three-dimensional vectors
+	 *  and additional vectors along the extra dimensions are setup to unify
+	 *  the calculations for all dimensions between 1-3. */
+	std::vector<Vector3d> basisVectors;
+
+	/** Vectors pointing along the normal direction of the two other basis
+	 *  vectors, and with reciprocal normal length of the corresponding
+	 *  basis vector. A scalar product between a coordinate vector and this
+	 *  vector gives the number of the corresponding basis vectors that is
+	 *  needed to reach the coordinate. */
+	std::vector<Vector3d> reciprocalNormals;
+};
+
+};	//End namespace TBTK
+
+#endif
---
+++ Lib/src/Uncategorized/WignerSeitzCell.cpp
@@ -0,0 +1,135 @@
+/* Copyright 2016 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file WignerSeitzCell.cpp
+ *
+ *  @author Kristofer Björnson
+ */
+
+#include "WignerSeitzCell.h"
+#include "TBTKMacros.h"
+
+using namespace std;
+
+namespace TBTK{
+
+WignerSeitzCell::WignerSeitzCell(const vector<vector<double>> &basisVectors){
+	this->dimensions = basisVectors.size();
+
+	TBTKAssert(
+		dimensions == 1
+		|| dimensions == 2
+		|| dimensions == 3,
+		"WignerSeitzCell::WignerSeitzCell()",
+		"Basis dimension not supported.",
+		"Only 1-3 basis vectors are supported, but "
+		<< basisVectors.size() << " basis vectors supplied."
+	);
+
+	for(unsigned int n = 0; n < dimensions; n++){
+		TBTKAssert(
+			basisVectors.at(n).size() == dimensions,
+			"WignerSeitzCell::WignerSeitzCell()",
+			"Incompatible dimensions.",
+			"The number of basis vectors must agree with the number"
+			<< " of components of the basis vectors. The number of"
+			<< "basis vectors are '" << dimensions << "',"
+			<< " but encountered basis vector with '"
+			<< basisVectors.at(n).size() << "' components."
+		);
+
+		vector<double> paddedBasisVector;
+		for(unsigned int c = 0; c < dimensions; c++)
+			paddedBasisVector.push_back(basisVectors.at(n).at(c));
+		for(unsigned int c = dimensions; c < 3; c++)
+			paddedBasisVector.push_back(0.);
+
+		this->basisVectors.push_back(Vector3d(paddedBasisVector));
+	}
+	for(unsigned int n = dimensions; n < 3; n++){
+		vector<double> vector;
+		for(unsigned int c = 0; c < 3; c++){
+			if(c == n)
+				vector.push_back(1.);
+			else
+				vector.push_back(0.);
+		}
+
+		this->basisVectors.push_back(Vector3d(vector));
+	}
+
+	for(unsigned int n = 0; n < 3; n++){
+		const Vector3d &v0 = basisVectors.at(n);
+		const Vector3d &v1 = basisVectors.at((n+1)%3);
+		const Vector3d &v2 = basisVectors.at((n+2)%3);
+
+		Vector3d normal = v1*v2;
+		normal = normal/Vector3d::dotProduct(normal, v0);
+
+		reciprocalNormals.push_back(normal);
+	}
+}
+
+vector<double> WignerSeitzCell::getCellIndex(initializer_list<double> coordinates) const{
+	TBTKAssert(
+		coordinates.size() == dimensions,
+		"WignerSeitzCell::getCellIndex()",
+		"Incompatible dimensions.",
+		"The number of coordinate components must agree with the"
+		<< " dimension of the Wigner-Seitz cell. The Weigner-Seitz"
+		<< " cell has " << dimensions << " dimensions, but coordinate"
+		<< " with " << coordinates.size() << " components supplied."
+	);
+
+	Vector3d coordinateVector;
+	switch(coordinates.size()){
+	case 1:
+		coordinateVector = Vector3d({
+			*(coordinates.begin() + 0),
+			0.,
+			0.
+		});
+		break;
+	case 2:
+		coordinateVector = Vector3d({
+			*(coordinates.begin() + 0),
+			*(coordinates.begin() + 1),
+			0.
+		});
+		break;
+	case 3:
+		coordinateVector = Vector3d({
+			*(coordinates.begin() + 0),
+			*(coordinates.begin() + 1),
+			*(coordinates.begin() + 2)
+		});
+		break;
+	default:
+		TBTKExit(
+			"WignerSeitzCell::WignerSeitzCell()",
+			"This should never happen.",
+			"Notify the developer about this bug."
+		);
+		break;
+	}
+
+	vector<double> cellIndex;
+	for(int n = 0; n < 3; n++)
+		cellIndex.push_back(Vector3d::dotProduct(coordinateVector, reciprocalNormals.at(n)));
+
+	return cellIndex;
+}
+
+};	//End of namespace TBTK
