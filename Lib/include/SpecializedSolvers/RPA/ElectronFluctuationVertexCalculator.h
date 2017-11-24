5bda32e6ac6189e98481a516a25790f69bc8b65d
Kristofer Björnson <dafer45@hotmail.com>
2017-11-24 15:59:10 +0100

    Began migration of vertex calculation from SelfEnergyCalculator to ElectronFluctuationVertexCalculator.

---
+++ Lib/include/SpecializedSolvers/RPA/ElectronFluctuationVertexCalculator.h
@@ -0,0 +1,253 @@
+/* Copyright 2017 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @package TBTKcalc
+ *  @file SelfEnergyCalculator.h
+ *  @brief Calculates the self-energy using the RPA approximation.
+ *
+ *  @author Kristofer Björnson
+ */
+
+#ifndef COM_DAFER45_TBTK_ELECTRON_FLUCTUATION_VERTEX_CALCULATOR
+#define COM_DAFER45_TBTK_ELECTRON_FLUCTUATION_VERTEX_CALCULATOR
+
+#include "BrillouinZone.h"
+#include "BlockDiagonalizationSolver.h"
+#include "BPropertyExtractor.h"
+#include "IndexedDataTree.h"
+#include "SusceptibilityCalculator.h"
+
+namespace TBTK{
+
+class ElectronFluctuationVertexCalculator{
+public:
+	/** Constructor. */
+	ElectronFluctuationVertexCalculator(
+		const MomentumSpaceContext &momentumSpaceContext
+	);
+
+	/** Destructor. */
+	~ElectronFluctuationVertexCalculator();
+
+	/** Create slave ElectronFluctuationVertexCalculator. The slave reuses
+	 *  internal lookup tables used to speed up the calculations and should
+	 *  not be used after the generating master have been destructed. */
+	ElectronFluctuationVertexCalculator* createSlave();
+
+	/** Get momentum cpsace context. */
+	const MomentumSpaceContext& getMomentumSpaceContext() const;
+
+	/** Initialize the SelfEnergyCalculator. */
+	void init();
+
+	/** Enum class for indicating whether the energy is an arbitrary comlex
+	 *  number, or if it is restricted to the real or imaginary axis. */
+	enum class EnergyType {Real, Imaginary, Complex};
+
+	/** Set self-energy energy type. */
+	void setEnergyType(EnergyType energyType);
+
+	/** Get energy type. */
+	EnergyType getEnergyType() const;
+
+	/** Set the energies for which the self-energy should be
+	 *  calculated. */
+	void setEnergies(
+		const std::vector<std::complex<double>> &energies
+	);
+
+	/** Calculate self-energy vertex. */
+	std::vector<std::complex<double>> calculateSelfEnergyVertex(
+		const std::vector<double> &k,
+		const std::vector<int> &orbitalIndices
+	);
+
+	/** Set U. */
+	void setU(std::complex<double> U);
+
+	/** Set Up. */
+	void setUp(std::complex<double> Up);
+
+	/** Set J. */
+	void setJ(std::complex<double> J);
+
+	/** Set Jp. */
+	void setJp(std::complex<double> Jp);
+
+	/** Save susceptibilities. */
+	void saveSusceptibilities(const std::string &filename) const;
+
+	/** Load susceptibilities. */
+	void loadSusceptibilities(const std::string &filename);
+private:
+	/** SusceptibilityCalculator. */
+	SusceptibilityCalculator *susceptibilityCalculator;
+
+	/** Lookup table for calculating k+q. */
+//	int *kMinusQLookupTable;
+
+	/** Generate lookup table for the k-q linear index. Can be called
+	 *  repeatedly, and the lookup table is only generated once. */
+//	void generateKMinusQLookupTable();
+
+	/** Returns the linear index for k+q. */
+/*	template<bool useKPlusKLookupTable>
+	int getKMinusQLinearIndex(
+		unsigned int meshIndex,
+		const std::vector<double> &k,
+		int kLinearIndex
+	) const;*/
+
+	/** Number of energies to sum over. */
+//	unsigned int numSummationEnergies;
+
+	/** Energies to sum over. */
+//	std::vector<std::complex<double>> summationEnergies;
+
+	/** Energy type. */
+	EnergyType energyType;
+
+	/** Energies to calculate the vertex for. */
+	std::vector<std::complex<double>> energies;
+
+	/** Flag indicating whether the SelfEnergyCalculator is initialized. */
+	bool isInitialized;
+
+	/** IndexedDataTree storing the vertex. */
+	IndexedDataTree<SerializeableVector<std::complex<double>>> vertexTree;
+
+	/** Invert matix. */
+/*	void invertMatrix(
+		std::complex<double> *matrix,
+		unsigned int dimensions
+	);*/
+
+	/** Multiply matrices. */
+/*	void multiplyMatrices(
+		std::complex<double> *matrix1,
+		std::complex<double> *matrix2,
+		std::complex<double> *result,
+		unsigned int dimensions
+	);*/
+
+	/** Print matrix. */
+/*	void printMatrix(
+		std::complex<double> *matrix,
+		unsigned int dimensions
+	);*/
+
+	/** Self-energy main loop. */
+/*	template<bool singleSelfEnergyEnergy>
+	void selfEnergyMainLoop(
+		const std::vector<double> &k,
+		const std::vector<int> &orbitalIndices,
+		std::vector<std::complex<double>> &result
+	);*/
+
+	/** Interaction parameters. */
+	std::complex<double> U, Up, J, Jp;
+
+	/** Interaction amplitudes used to calculate the self-energy vertex. */
+	std::vector<InteractionAmplitude> u1;
+	std::vector<InteractionAmplitude> u2;
+	std::vector<InteractionAmplitude> u3;
+
+	/** Flag indicating whether the interaction amplitudes are initialized.
+	 */
+	bool interactionAmplitudesAreGenerated;
+
+	/** Generate interaction amplitudes. Can be called multiple times and
+	 *  will only regenerate the interaction amplitudes when needed. */
+	void generateInteractionAmplitudes();
+};
+
+inline const MomentumSpaceContext& ElectronFluctuationVertexCalculator::getMomentumSpaceContext(
+) const{
+	return susceptibilityCalculator->getMomentumSpaceContext();
+}
+
+/*inline void SelfEnergyCalculator::setNumSummationEnergies(
+	unsigned int numSummationEnergies
+){
+	this->numSummationEnergies = numSummationEnergies;
+}*/
+
+inline void ElectronFluctuationVertexCalculator::setEnergyType(
+	EnergyType energyType
+){
+	energyType = energyType;
+}
+
+inline ElectronFluctuationVertexCalculator::EnergyType ElectronFluctuationVertexCalculator::getEnergyType(
+) const{
+	return energyType;
+}
+
+inline void ElectronFluctuationVertexCalculator::setEnergies(
+	const std::vector<std::complex<double>> &energies
+){
+	this->energies = energies;
+	vertexTree.clear();
+}
+
+inline void ElectronFluctuationVertexCalculator::setU(std::complex<double> U){
+	this->U = U;
+	susceptibilityCalculator->setU(U);
+	vertexTree.clear();
+	interactionAmplitudesAreGenerated = false;
+}
+
+inline void ElectronFluctuationVertexCalculator::setUp(std::complex<double> Up){
+	this->Up = Up;
+	susceptibilityCalculator->setUp(Up);
+	vertexTree.clear();
+	interactionAmplitudesAreGenerated = false;
+}
+
+inline void ElectronFluctuationVertexCalculator::setJ(std::complex<double> J){
+	this->J = J;
+	susceptibilityCalculator->setJ(J);
+	vertexTree.clear();
+	interactionAmplitudesAreGenerated = false;
+}
+
+inline void ElectronFluctuationVertexCalculator::setJp(std::complex<double> Jp){
+	this->Jp = Jp;
+	susceptibilityCalculator->setJp(Jp);
+	vertexTree.clear();
+	interactionAmplitudesAreGenerated = false;
+}
+
+/*inline void SelfEnergyCalculator::precomputeSusceptibilities(
+	unsigned int numWorkers
+){
+	susceptibilityCalculator.precompute(numWorkers);
+}*/
+
+inline void ElectronFluctuationVertexCalculator::saveSusceptibilities(
+	const std::string &filename
+) const{
+	susceptibilityCalculator->saveSusceptibilities(filename);
+}
+
+inline void ElectronFluctuationVertexCalculator::loadSusceptibilities(
+	const std::string &filename
+){
+	susceptibilityCalculator->loadSusceptibilities(filename);
+}
+
+};	//End of namespace TBTK
+
+#endif
---
+++ Lib/src/SpecializedSolvers/RPA/ElectronFluctuationVertexCalculator.cpp
@@ -0,0 +1,561 @@
+/* Copyright 2017 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file SelfEnergyCalculator.cpp
+ *
+ *  @author Kristofer Björnson
+ */
+
+#include "Functions.h"
+#include "InteractionAmplitude.h"
+#include "RPA/ElectronFluctuationVertexCalculator.h"
+#include "UnitHandler.h"
+
+#include <complex>
+#include <iomanip>
+
+using namespace std;
+
+const complex<double> i(0, 1);
+
+namespace TBTK{
+
+ElectronFluctuationVertexCalculator::ElectronFluctuationVertexCalculator(
+	const MomentumSpaceContext &momentumSpaceContext
+){
+	isInitialized = false;
+
+//	kMinusQLookupTable = nullptr;
+
+	U = 0.;
+	Up = 0.;
+	J = 0.;
+	Jp = 0.;
+
+	susceptibilityCalculator = new SusceptibilityCalculator(momentumSpaceContext);
+/*	for(unsigned int n = 1; n < numWorkers; n++){
+		susceptibilityCalculators.push_back(
+			susceptibilityCalculators[0]->createSlave()
+		);
+	}*/
+
+	interactionAmplitudesAreGenerated = false;
+}
+
+ElectronFluctuationVertexCalculator::~ElectronFluctuationVertexCalculator(){
+	if(susceptibilityCalculator != nullptr)
+		delete susceptibilityCalculator;
+}
+
+ElectronFluctuationVertexCalculator* ElectronFluctuationVertexCalculator::createSlave(){
+	isInitialized = false;
+
+	U = 0.;
+	Up = 0.;
+	J = 0.;
+	Jp = 0.;
+
+	susceptibilityCalculator = susceptibilityCalculator->createSlave();
+
+	interactionAmplitudesAreGenerated = false;
+
+	return this;
+}
+
+void ElectronFluctuationVertexCalculator::init(){
+	//Calculate kT
+/*	double temperature = UnitHandler::convertTemperatureNtB(
+		susceptibilityCalculators[0]->getMomentumSpaceContext(
+		).getModel().getTemperature()
+	);
+	double kT = UnitHandler::getK_BB()*temperature;
+
+	//Initialize summation energies
+	for(
+		int n = -(int)numSummationEnergies/2;
+		n <= (int)numSummationEnergies/2;
+		n++
+	){
+		summationEnergies.push_back(i*M_PI*2.*(double)(n)*kT);
+	}
+	for(unsigned int n = 0; n < susceptibilityCalculators.size(); n++){
+		susceptibilityCalculators[n]->setSusceptibilityEnergies(
+			summationEnergies
+		);
+		susceptibilityCalculators[n]->setSusceptibilityEnergyType(
+			SusceptibilityCalculator::EnergyType::Imaginary
+		);
+		susceptibilityCalculators[n]->setSusceptibilityEnergiesAreInversionSymmetric(
+			true
+		);
+	}*/
+
+/*	Timer::tick("Precompute");
+	susceptibilityCalculator.precompute();
+	Timer::tock();*/
+
+	isInitialized = true;
+}
+
+void ElectronFluctuationVertexCalculator::generateInteractionAmplitudes(){
+	if(interactionAmplitudesAreGenerated)
+		return;
+
+	u1.clear();
+	u2.clear();
+	u3.clear();
+
+	const MomentumSpaceContext &momentumSpaceContext = susceptibilityCalculator->getMomentumSpaceContext();
+	unsigned int numOrbitals = momentumSpaceContext.getNumOrbitals();
+
+	for(int a = 0; a < (int)numOrbitals; a++){
+		u2.push_back(
+			InteractionAmplitude(
+				U,
+				{{a},	{a}},
+				{{a},	{a}}
+			)
+		);
+		u3.push_back(
+			InteractionAmplitude(
+				-U,
+				{{a},	{a}},
+				{{a},	{a}}
+			)
+		);
+
+		for(int b = 0; b < (int)numOrbitals; b++){
+			if(a == b)
+				continue;
+
+			u1.push_back(
+				InteractionAmplitude(
+					Up - J,
+					{{a},	{b}},
+					{{b},	{a}}
+				)
+			);
+			u2.push_back(
+				InteractionAmplitude(
+					Up,
+					{{a},	{b}},
+					{{b},	{a}}
+				)
+			);
+			u3.push_back(
+				InteractionAmplitude(
+					-J,
+					{{a},	{b}},
+					{{b},	{a}}
+				)
+			);
+
+			u1.push_back(
+				InteractionAmplitude(
+					J - Up,
+					{{a},	{b}},
+					{{a},	{b}}
+				)
+			);
+			u2.push_back(
+				InteractionAmplitude(
+					J,
+					{{a},	{b}},
+					{{a},	{b}}
+				)
+			);
+			u3.push_back(
+				InteractionAmplitude(
+					-Up,
+					{{a},	{b}},
+					{{a},	{b}}
+				)
+			);
+
+			u2.push_back(
+				InteractionAmplitude(
+					Jp,
+					{{a},	{a}},
+					{{b},	{b}}
+				)
+			);
+			u3.push_back(
+				InteractionAmplitude(
+					-Jp,
+					{{a},	{a}},
+					{{b},	{b}}
+				)
+			);
+		}
+	}
+
+	interactionAmplitudesAreGenerated = true;
+}
+
+vector<complex<double>> ElectronFluctuationVertexCalculator::calculateSelfEnergyVertex(
+	const vector<double> &k,
+	const vector<int> &orbitalIndices
+){
+	TBTKAssert(
+		isInitialized,
+		"ElectronFLuctuationVertexCalculator::calculateSelfEnergyVertex()",
+		"ElectronFluctuationVertexCalculator not yet initialized.",
+		"Use ElectronFluctuationVertexCalculator::init() to initialize"
+		<< " the SelfEnergyCalculator."
+	);
+	TBTKAssert(
+		orbitalIndices.size() == 4,
+		"ElectronFluctuationVertexCalculator::calculateSelfEnergyVertex()",
+		"Two orbital indices required but " << orbitalIndices.size()
+		<< " supplied.",
+		""
+	);
+
+	const MomentumSpaceContext &momentumSpaceContext = susceptibilityCalculator->getMomentumSpaceContext();
+	const BrillouinZone &brillouinZone = momentumSpaceContext.getBrillouinZone();
+	const vector<unsigned int> &numMeshPoints = momentumSpaceContext.getNumMeshPoints();
+
+	//Calculate offset
+	Index kIndex = brillouinZone.getMinorCellIndex(
+		k,
+		numMeshPoints
+	);
+
+	Index resultIndex = Index(
+		kIndex,
+		{
+			orbitalIndices.at(0),
+			orbitalIndices.at(1),
+			orbitalIndices.at(2),
+			orbitalIndices.at(3)
+		}
+	);
+
+	SerializeableVector<complex<double>> result;
+	if(vertexTree.get(result, resultIndex))
+		return result;
+
+	DualIndex kDual(kIndex, k);
+
+//	generateInteractionAmplitudes();
+
+	vector<complex<double>> selfEnergyVertex;
+	selfEnergyVertex.reserve(energies.size());
+	for(
+		unsigned int n = 0;
+		n < energies.size();
+		n++
+	){
+		selfEnergyVertex.push_back(0.);
+	}
+
+	//U_1*\chi_1*U_1
+	for(unsigned int in = 0; in < u1.size(); in++){
+		const InteractionAmplitude &incommingAmplitude = u1.at(in);
+		complex<double> amplitude_i = incommingAmplitude.getAmplitude();
+		int c0_i = incommingAmplitude.getCreationOperatorIndex(0).at(0);
+		int c1_i = incommingAmplitude.getCreationOperatorIndex(1).at(0);
+		int a0_i = incommingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+		int a1_i = incommingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+		if(
+/*			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(0)*/
+			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(2)
+			|| abs(amplitude_i) < 1e-10
+		){
+			continue;
+		}
+
+		for(unsigned int out = 0; out < u1.size(); out++){
+			const InteractionAmplitude &outgoingAmplitude = u1.at(out);
+			complex<double> amplitude_o = outgoingAmplitude.getAmplitude();
+			int c0_o = outgoingAmplitude.getCreationOperatorIndex(0).at(0);
+			int c1_o = outgoingAmplitude.getCreationOperatorIndex(1).at(0);
+			int a0_o = outgoingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+			int a1_o = outgoingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+			if(
+/*				a0_o != orbitalIndices.at(2)
+				|| c1_o != orbitalIndices.at(1)*/
+				a0_o != orbitalIndices.at(0)
+				|| c1_o != orbitalIndices.at(1)
+				|| abs(amplitude_o) < 1e-10
+			){
+				continue;
+			}
+
+			vector<complex<double>> chargeSusceptibility = susceptibilityCalculator->calculateChargeRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			vector<complex<double>> spinSusceptibility = susceptibilityCalculator->calculateSpinRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			for(
+				unsigned int n = 0;
+				n < energies.size();
+				n++
+			){
+				selfEnergyVertex.at(n) += amplitude_i*amplitude_o*(
+					chargeSusceptibility.at(n)
+					+ spinSusceptibility.at(n)
+				)/2.;
+			}
+		}
+	}
+
+	//U_2*\chi_1*U_2
+	for(unsigned int in = 0; in < u2.size(); in++){
+		const InteractionAmplitude &incommingAmplitude = u2.at(in);
+		complex<double> amplitude_i = incommingAmplitude.getAmplitude();
+		int c0_i = incommingAmplitude.getCreationOperatorIndex(0).at(0);
+		int c1_i = incommingAmplitude.getCreationOperatorIndex(1).at(0);
+		int a0_i = incommingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+		int a1_i = incommingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+		if(
+/*			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(0)*/
+			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(2)
+			|| abs(amplitude_i) < 1e-10
+		){
+			continue;
+		}
+
+		for(unsigned int out = 0; out < u2.size(); out++){
+			const InteractionAmplitude &outgoingAmplitude = u2.at(out);
+			complex<double> amplitude_o = outgoingAmplitude.getAmplitude();
+			int c0_o = outgoingAmplitude.getCreationOperatorIndex(0).at(0);
+			int c1_o = outgoingAmplitude.getCreationOperatorIndex(1).at(0);
+			int a0_o = outgoingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+			int a1_o = outgoingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+			if(
+/*				a0_o != orbitalIndices.at(2)
+				|| c1_o != orbitalIndices.at(1)*/
+				a0_o != orbitalIndices.at(0)
+				|| c1_o != orbitalIndices.at(1)
+				|| abs(amplitude_o) < 1e-10
+			){
+				continue;
+			}
+
+			vector<complex<double>> chargeSusceptibility = susceptibilityCalculator->calculateChargeRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			vector<complex<double>> spinSusceptibility = susceptibilityCalculator->calculateSpinRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			for(
+				unsigned int n = 0;
+				n < energies.size();
+				n++
+			){
+				selfEnergyVertex.at(n) += amplitude_i*amplitude_o*(
+					chargeSusceptibility.at(n)
+					+ spinSusceptibility.at(n)
+				)/2.;
+			}
+		}
+	}
+
+	//U_1*\chi_2*U_2
+	for(unsigned int in = 0; in < u1.size(); in++){
+		const InteractionAmplitude &incommingAmplitude = u1.at(in);
+		complex<double> amplitude_i = incommingAmplitude.getAmplitude();
+		int c0_i = incommingAmplitude.getCreationOperatorIndex(0).at(0);
+		int c1_i = incommingAmplitude.getCreationOperatorIndex(1).at(0);
+		int a0_i = incommingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+		int a1_i = incommingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+		if(
+/*			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(0)*/
+			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(2)
+			|| abs(amplitude_i) < 1e-10
+		){
+			continue;
+		}
+
+		for(unsigned int out = 0; out < u2.size(); out++){
+			const InteractionAmplitude &outgoingAmplitude = u2.at(out);
+			complex<double> amplitude_o = outgoingAmplitude.getAmplitude();
+			int c0_o = outgoingAmplitude.getCreationOperatorIndex(0).at(0);
+			int c1_o = outgoingAmplitude.getCreationOperatorIndex(1).at(0);
+			int a0_o = outgoingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+			int a1_o = outgoingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+			if(
+/*				a0_o != orbitalIndices.at(2)
+				|| c1_o != orbitalIndices.at(1)*/
+				a0_o != orbitalIndices.at(0)
+				|| c1_o != orbitalIndices.at(1)
+				|| abs(amplitude_o) < 1e-10
+			){
+				continue;
+			}
+
+			vector<complex<double>> chargeSusceptibility = susceptibilityCalculator->calculateChargeRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			vector<complex<double>> spinSusceptibility = susceptibilityCalculator->calculateSpinRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			for(
+				unsigned int n = 0;
+				n < energies.size();
+				n++
+			){
+				selfEnergyVertex.at(n) += amplitude_i*amplitude_o*(
+					chargeSusceptibility.at(n)
+					- spinSusceptibility.at(n)
+				)/2.;
+			}
+		}
+	}
+
+	//U_2*\chi_2*U_1
+	for(unsigned int in = 0; in < u2.size(); in++){
+		const InteractionAmplitude &incommingAmplitude = u2.at(in);
+		complex<double> amplitude_i = incommingAmplitude.getAmplitude();
+		int c0_i = incommingAmplitude.getCreationOperatorIndex(0).at(0);
+		int c1_i = incommingAmplitude.getCreationOperatorIndex(1).at(0);
+		int a0_i = incommingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+		int a1_i = incommingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+		if(
+/*			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(0)*/
+			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(2)
+			|| abs(amplitude_i) < 1e-10
+		){
+			continue;
+		}
+
+		for(unsigned int out = 0; out < u1.size(); out++){
+			const InteractionAmplitude &outgoingAmplitude = u1.at(out);
+			complex<double> amplitude_o = outgoingAmplitude.getAmplitude();
+			int c0_o = outgoingAmplitude.getCreationOperatorIndex(0).at(0);
+			int c1_o = outgoingAmplitude.getCreationOperatorIndex(1).at(0);
+			int a0_o = outgoingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+			int a1_o = outgoingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+			if(
+/*				a0_o != orbitalIndices.at(2)
+				|| c1_o != orbitalIndices.at(1)*/
+				a0_o != orbitalIndices.at(0)
+				|| c1_o != orbitalIndices.at(1)
+				|| abs(amplitude_o) < 1e-10
+			){
+				continue;
+			}
+
+			vector<complex<double>> chargeSusceptibility = susceptibilityCalculator->calculateChargeRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			vector<complex<double>> spinSusceptibility = susceptibilityCalculator->calculateSpinRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			for(
+				unsigned int n = 0;
+				n < energies.size();
+				n++
+			){
+				selfEnergyVertex.at(n) += amplitude_i*amplitude_o*(
+					chargeSusceptibility.at(n)
+					- spinSusceptibility.at(n)
+				)/2.;
+			}
+		}
+	}
+
+	//U_3*\chi_3*U_3
+	for(unsigned int in = 0; in < u3.size(); in++){
+		const InteractionAmplitude &incommingAmplitude = u3.at(in);
+		complex<double> amplitude_i = incommingAmplitude.getAmplitude();
+		int c0_i = incommingAmplitude.getCreationOperatorIndex(0).at(0);
+		int c1_i = incommingAmplitude.getCreationOperatorIndex(1).at(0);
+		int a0_i = incommingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+		int a1_i = incommingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+		if(
+/*			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(0)*/
+			a1_i != orbitalIndices.at(3)
+			|| c0_i != orbitalIndices.at(2)
+			|| abs(amplitude_i) < 1e-10
+		){
+			continue;
+		}
+
+		for(unsigned int out = 0; out < u3.size(); out++){
+			const InteractionAmplitude &outgoingAmplitude = u3.at(out);
+			complex<double> amplitude_o = outgoingAmplitude.getAmplitude();
+			int c0_o = outgoingAmplitude.getCreationOperatorIndex(0).at(0);
+			int c1_o = outgoingAmplitude.getCreationOperatorIndex(1).at(0);
+			int a0_o = outgoingAmplitude.getAnnihilationOperatorIndex(0).at(0);
+			int a1_o = outgoingAmplitude.getAnnihilationOperatorIndex(1).at(0);
+
+			if(
+/*				a0_o != orbitalIndices.at(2)
+				|| c1_o != orbitalIndices.at(1)*/
+				a0_o != orbitalIndices.at(0)
+				|| c1_o != orbitalIndices.at(1)
+				|| abs(amplitude_o) < 1e-10
+			){
+				continue;
+			}
+
+			vector<complex<double>> spinSusceptibility = susceptibilityCalculator->calculateSpinRPASusceptibility(
+				kDual,
+//				{c1_i, a0_i, c0_o, a1_o}
+				{c0_o, a1_o, c1_i, a0_i}
+			);
+			for(
+				unsigned int n = 0;
+				n < energies.size();
+				n++
+			){
+				selfEnergyVertex.at(n) += amplitude_i*amplitude_o*spinSusceptibility.at(n);
+			}
+		}
+	}
+
+	vertexTree.add(selfEnergyVertex, resultIndex);
+
+	return selfEnergyVertex;
+}
+
+}	//End of namesapce TBTK
