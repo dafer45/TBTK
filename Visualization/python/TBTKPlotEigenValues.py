79c5ff60cff8a9871e60219775778db3e5e3a919
Kristofer Björnson <dafer45@hotmail.com>
2018-03-05 02:45:13 +0100

    Updated build scripts.

--- Visualization/python/TBTKPlotEigenValues.py
+++ Visualization/python/TBTKPlotEigenValues.py
@@ -16,5 +16,6 @@ if(len(sys.argv) != 2):
 	print "Error, the following arguments are needed: .hdf5-file"
 	exit(1)
 
-os.system("python " + os.environ['TBTK_dir'] + "/Visualization/python/TBTKPlot1D.py " + sys.argv[1] + " EigenValues")
+#os.system("python " + os.environ['TBTK_dir'] + "/Visualization/python/TBTKPlot1D.py " + sys.argv[1] + " EigenValues")
+os.system("${CMAKE_INSTALL_PREFIX}/bin/TBTKPlot1D.py " + sys.argv[1] + " EigenValues")
 
