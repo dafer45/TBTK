7d71b6c7d4e4e27efc533861f0ebd32901407e8e
Kristofer Björnson <dafer45@hotmail.com>
2018-03-07 15:49:09 +0100

    Removed old build files.

--- hdf5/fetchAndBuild.sh
+++
@@ -1,8 +0,0 @@
-wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.16/src/hdf5-1.8.16.tar
-tar -xvf hdf5-1.8.16.tar
-rm hdf5-1.8.16.tar
-mkdir hdf5-build
-	CFLAGS="-std=c++11"
-	cd hdf5-build; ../hdf5-1.8.16/configure --enable-cxx
-	cd hdf5-build; make
-	cd hdf5-build; make install
