a92669aaa50ebec944251cf57903fcbf8d182fd3
Kristofer Björnson <dafer45@hotmail.com>
2018-04-03 12:07:45 +0200

    Began adding tests for IndexTree.

--- Test/include/Core/TBTK/Test/IndexTree.h
+++ Test/include/Core/TBTK/Test/IndexTree.h
@@ -1,22 +1,21 @@
-#include "TBTK/IndexedDataTree.h"
-#include "TBTK/Model.h"
+#include "TBTK/IndexTree.h"
 
 #include "gtest/gtest.h"
 
 namespace TBTK{
 
-TEST(IndexedDataTree, Constructor){
+TEST(IndexTree, Constructor){
 	//Not testable on its own.
 }
 
 //TODO
 //...
-TEST(IndexedDataTree, SerializeToJSON){
+//TEST(IndexedDataTree, SerializeToJSON){
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
 
-	Streams::setStdMuteOut();
+/*	Streams::setStdMuteOut();
 
 	//Setup Models that will work as test elements.
 	Model model0;
@@ -60,12 +59,12 @@ TEST(IndexedDataTree, SerializeToJSON){
 
 	//Access non-existing element
 	EXPECT_THROW(indexedDataTree1.get({1, 2, 2}), ElementNotFoundException);
-	EXPECT_THROW(indexedDataTree1.get({1, 1, 3}), ElementNotFoundException);
+	EXPECT_THROW(indexedDataTree1.get({1, 1, 3}), ElementNotFoundException);*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree2;
+/*	IndexedDataTree<int> indexedDataTree2;
 	indexedDataTree2.add(1, {1, 2, 3});
 	indexedDataTree2.add(2, {1, 2, 4});
 	indexedDataTree2.add(3, {1, 2, 3});
@@ -84,79 +83,79 @@ TEST(IndexedDataTree, SerializeToJSON){
 	//Access non-existing element
 	EXPECT_THROW(indexedDataTree3.get({1, 2, 2}), ElementNotFoundException);
 	EXPECT_THROW(indexedDataTree3.get({1, 1, 3}), ElementNotFoundException);
+}*/
+
+TEST(IndexTree, Destructor){
+	//Not testable on its own.
 }
 
-TEST(IndexedDataTree, add){
-	/**************************/
-	/* Serializable elements. */
-	/**************************/
-	IndexedDataTree<Model> indexedDataTree0;
+TEST(IndexTree, add){
+	IndexTree indexTree;
 
-	//Adding elements with normal Index.
-	indexedDataTree0.add(Model(), {1, 2, 3});
-	indexedDataTree0.add(Model(), {1, 2, 4});
+	//Adding normal Index.
+	indexTree.add({1, 2, 3});
+	indexTree.add({1, 2, 4});
 
-	//Adding element to already existing element (overwrite).
-	indexedDataTree0.add(Model(), {1, 2, 3});
+	//Adding already existing Index.
+	indexTree.add({1, 2, 3});
 
-	//Adding elements with multiple indices.
-	indexedDataTree0.add(Model(), {{1, 2, 5}, {1, 2, 3}});
-	indexedDataTree0.add(Model(), {{1, 2, 5}, {1, 2, 4}});
+	//Adding compund Index.
+	indexTree.add({{1, 2, 5}, {1, 2, 3}});
+	indexTree.add({{1, 2, 5}, {1, 2, 4}});
 
-	//Adding element to already existing element (overwrite) for element
-	//with multiple indices.
-	indexedDataTree0.add(Model(), {{1, 2, 5}, {1, 2, 3}});
+	//Adding already existing compund Index.
+	indexTree.add({{1, 2, 5}, {1, 2, 3}});
 
-	//Fail to add element with multiple indices if an element with a single
-	//Index with the same structure already have been added.
+	//Fail to add compund Index if a non-compund Index with the same
+	//structure already have been added.
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {{1, 2, 3}, {1, 2, 3}});
+			indexTree.add({{1, 2, 3}, {1, 2, 3}});
 		},
 		::testing::ExitedWithCode(1),
 		""
 	);
 
-	//Fail to add element with if the Index clashes with multiple Index
-	//structure that already have been added.
+	//Fail to add Index if it clashes with a compund Index structure that
+	//already have been added.
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {1, 2, 5});
+			indexTree.add({1, 2, 5});
 		},
 		::testing::ExitedWithCode(1),
 		""
 	);
 
-	//Fail to add element if the Index clashes with an Index structure that
+	//Fail to add Index if it clashes with an Index structure that
 	//already has been added (longer version).
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {1, 2, 3, 4});
+			indexTree.add({1, 2, 3, 4});
 		},
 		::testing::ExitedWithCode(1),
 		""
 	);
 
-	//Fail to add element if the Index clashes with an Index structure that
+	//Fail to add Index if it clashes with an Index structure that
 	//already has been added (shorter version).
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {1, 2});
+			indexTree.add({1, 2});
 		},
 		::testing::ExitedWithCode(1),
 		""
 	);
 
-	//Fail to add element if the Index clashes with multiple Index
-	//structure that already has been added.
+	//Fail to add Index if it clashes with a compund Index structure that
+	//already has been added.
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {1, 2, 5, 6});
+			indexTree.add({1, 2, 5, 6});
 		},
 		::testing::ExitedWithCode(1),
 		""
@@ -167,7 +166,7 @@ TEST(IndexedDataTree, add){
 	EXPECT_EXIT(
 		{
 			Streams::setStdMuteErr();
-			indexedDataTree0.add(Model(), {1, -1, 3});
+			indexTree.add({1, -1, 3});
 		},
 		::testing::ExitedWithCode(1),
 		""
@@ -176,7 +175,7 @@ TEST(IndexedDataTree, add){
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<double> indexedDataTree1;
+/*	IndexedDataTree<double> indexedDataTree1;
 
 	//Adding elements with normal Index.
 	indexedDataTree1.add(0, {1, 2, 3});
@@ -257,15 +256,15 @@ TEST(IndexedDataTree, add){
 		},
 		::testing::ExitedWithCode(1),
 		""
-	);
+	);*/
 }
 
-TEST(IndexedDataTree, get0){
+/*TEST(IndexedDataTree, get0){*/
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
 
-	Streams::setStdMuteOut();
+/*	Streams::setStdMuteOut();
 
 	//Setup Models that will work as test elements.
 	Model model0;
@@ -319,12 +318,12 @@ TEST(IndexedDataTree, get0){
 		},
 		::testing::ExitedWithCode(1),
 		""
-	);
+	);*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree1;
+/*	IndexedDataTree<int> indexedDataTree1;
 	indexedDataTree1.add(1, {1, 2, 3});
 	indexedDataTree1.add(2, {1, 2, 4});
 	indexedDataTree1.add(3, {1, 2, 3});
@@ -355,12 +354,12 @@ TEST(IndexedDataTree, get0){
 	);
 }
 
-TEST(IndexedDataTree, get1){
+TEST(IndexedDataTree, get1){*/
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
 
-	Streams::setStdMuteOut();
+/*	Streams::setStdMuteOut();
 
 	//Setup Models that will work as test elements.
 	Model model0;
@@ -410,12 +409,12 @@ TEST(IndexedDataTree, get1){
 		},
 		::testing::ExitedWithCode(1),
 		""
-	);
+	);*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree1;
+/*	IndexedDataTree<int> indexedDataTree1;
 	indexedDataTree1.add(1, {1, 2, 3});
 	indexedDataTree1.add(2, {1, 2, 4});
 	indexedDataTree1.add(3, {1, 2, 3});
@@ -443,11 +442,11 @@ TEST(IndexedDataTree, get1){
 	);
 }
 
-TEST(IndexedDataTree, clear){
+TEST(IndexedDataTree, clear){*/
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
-	IndexedDataTree<Model> indexedDataTree0;
+/*	IndexedDataTree<Model> indexedDataTree0;
 	indexedDataTree0.add(Model(), {1, 2, 3});
 	indexedDataTree0.add(Model(), {1, 2, 4});
 	indexedDataTree0.add(Model(), {{1, 2, 5}, {1, 2, 3}});
@@ -456,12 +455,12 @@ TEST(IndexedDataTree, clear){
 	Model model;
 	EXPECT_FALSE(indexedDataTree0.get(model, {1, 2, 3}));
 	EXPECT_FALSE(indexedDataTree0.get(model, {1, 2, 4}));
-	EXPECT_FALSE(indexedDataTree0.get(model, {{1, 2, 5}, {1, 2, 3}}));
+	EXPECT_FALSE(indexedDataTree0.get(model, {{1, 2, 5}, {1, 2, 3}}));*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree1;
+/*	IndexedDataTree<int> indexedDataTree1;
 	indexedDataTree1.add(1, {1, 2, 3});
 	indexedDataTree1.add(1, {1, 2, 4});
 	indexedDataTree1.add(1, {{1, 2, 5}, {1, 2, 3}});
@@ -473,27 +472,27 @@ TEST(IndexedDataTree, clear){
 	EXPECT_FALSE(indexedDataTree1.get(myInt, {{1, 2, 5}, {1, 2, 3}}));
 }
 
-TEST(IndexedDataTree, getSizeInBytes){
+TEST(IndexedDataTree, getSizeInBytes){*/
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
-	IndexedDataTree<Model> indexedDataTree0;
-	EXPECT_TRUE(indexedDataTree0.getSizeInBytes() > 0);
+/*	IndexedDataTree<Model> indexedDataTree0;
+	EXPECT_TRUE(indexedDataTree0.getSizeInBytes() > 0);*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree1;
+/*	IndexedDataTree<int> indexedDataTree1;
 	EXPECT_TRUE(indexedDataTree1.getSizeInBytes() > 0);
 }
 
-TEST(IndexedDataTree, Iterator){
+TEST(IndexedDataTree, Iterator){*/
 	/**************************/
 	/* Serializable elements. */
 	/**************************/
 
 	//Setup Models that will work as test elements.
-	Model model0;
+/*	Model model0;
 	model0 << HoppingAmplitude(1, {0}, {0});
 	model0.construct();
 
@@ -532,12 +531,12 @@ TEST(IndexedDataTree, Iterator){
 	EXPECT_EQ((*iterator0).getBasisSize(), 4);
 	++iterator0;
 	EXPECT_TRUE(iterator0 == indexedDataTree0.end());
-	EXPECT_FALSE(iterator0 != indexedDataTree0.end());
+	EXPECT_FALSE(iterator0 != indexedDataTree0.end());*/
 
 	/*************************************/
 	/* Non/pseudo-serializable elements. */
 	/*************************************/
-	IndexedDataTree<int> indexedDataTree1;
+/*	IndexedDataTree<int> indexedDataTree1;
 	indexedDataTree1.add(1, {1, 2, 3});
 	indexedDataTree1.add(2, {1, 2, 4});
 	indexedDataTree1.add(3, {1, 2, 3});
@@ -561,6 +560,6 @@ TEST(IndexedDataTree, Iterator){
 	++iterator1;
 	EXPECT_TRUE(iterator1 == indexedDataTree1.end());
 	EXPECT_FALSE(iterator1 != indexedDataTree1.end());
-}
+}*/
 
 };
