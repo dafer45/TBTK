ea6ff288427e50caddacff41402018ca1c75a504
Kristofer Björnson <dafer45@hotmail.com>
2018-05-07 00:28:22 +0200

    Added Property::InteractionVertex.

---
+++ Lib/include/TBTK/Property/InteractionVertex.h
@@ -0,0 +1,120 @@
+/* Copyright 2018 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @package TBTKcalc
+ *  @file InteractionVertex.h
+ *  @brief Property container for an InteractionVertex.
+ *
+ *  @author Kristofer Björnson
+ */
+
+#ifndef COM_DAFER45_TBTK_PROPERTY_INTERACTION_VERTEX
+#define COM_DAFER45_TBTK_PROPERTY_INTERACTION_VERTEX
+
+#include "TBTK/Property/EnergyResolvedProperty.h"
+#include "TBTK/TBTKMacros.h"
+
+#include <complex>
+#include <vector>
+
+namespace TBTK{
+namespace Property{
+
+/** @brief Property container for the Susceptibility. */
+class InteractionVertex : public EnergyResolvedProperty<std::complex<double>>{
+public:
+	/** Constructs an uninitialized InteractionVertex. */
+//	InteractionVertex();
+
+	/** Constructs an InteractionVertex with real energies on the Custom
+	 *  format. [See AbstractProperty for detailed information about the
+	 *  Custom format.]
+	 *
+	 *  @param indexTree IndexTree containing the @link Index Indices
+	 *  @endlink for which the Susceptibility should be contained.
+	 *
+	 *  @param lowerBound Lower bound for the energy.
+	 *  @param upperBound Upper bound for the energy.
+	 *  @param resolution Number of points to use for the energy. */
+	InteractionVertex(
+		const IndexTree &indexTree,
+		double lowerBound,
+		double upperBound,
+		unsigned int resolution
+	);
+
+	/** Constructs an InteractionVertex with real energies on the Custom
+	 *  format and initializes it with data. [See AbstractProperty for
+	 *  detailed information about the Custom format and the raw data
+	 *  format.]
+	 *
+	 *  @param indexTree IndexTree containing the @link Index Indices
+	 *  @endlink for which the InteractionVertex should be contained.
+	 *
+	 *  @param lowerBound Lower bound for the energy.
+	 *  @param upperBound Upper bound for the energy.
+	 *  @param resolution Number of points to use for the energy.
+	 *  @param data Raw data to initialize the InteractionVertex with. */
+	InteractionVertex(
+		const IndexTree &indexTree,
+		double lowerBound,
+		double upperBound,
+		unsigned int resolution,
+		const std::complex<double> *data
+	);
+
+	/** Constructs an InteractionVertex with Matsubara energies on the
+	 *  Custom format. [See AbstractProperty for detailed information about
+	 *  the Custom format.]
+	 *
+	 *  @param indexTree IndexTree containing the @link Index Indices
+	 *  @endlink for which the InteractionVertex should be contained.
+	 *
+	 *  @param lowerBound Lower bound for the energy.
+	 *  @param upperBound Upper bound for the energy.
+	 *  @param resolution Number of points to use for the energy. */
+	InteractionVertex(
+		const IndexTree &indexTree,
+		int lowerMatsubaraEnergyIndex,
+		int upperMatsubaraEnergyIndex,
+		double fundamentalMatsubaraEnergy
+	);
+
+	/** Constructs an InteractionVertex with Matsubara energies on the
+	 *  Custom format and initializes it with data. [See AbstractProperty
+	 *  for detailed information about the Custom format and the raw data
+	 *  format.]
+	 *
+	 *  @param indexTree IndexTree containing the @link Index Indices
+	 *  @endlink for which the InteractionVertex should be contained.
+	 *
+	 *  @param lowerBound Lower bound for the energy.
+	 *  @param upperBound Upper bound for the energy.
+	 *  @param resolution Number of points to use for the energy.
+	 *  @param data Raw data to initialize the GreensFunction with. */
+	InteractionVertex(
+		const IndexTree &indexTree,
+		int lowerMatsubaraEnergyIndex,
+		int upperMatsubaraEnergyIndex,
+		double fundamentalMatsubaraEnergy,
+		const std::complex<double> *data
+	);
+private:
+};
+
+};	//End namespace Property
+};	//End namespace TBTK
+
+#endif
---
+++ Lib/src/Property/InteractionVertex.cpp
@@ -0,0 +1,88 @@
+/* Copyright 2018 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file InteractionVertex.cpp
+ *
+ *  @author Kristofer Björnson
+ */
+
+#include "TBTK/Property/InteractionVertex.h"
+#include "TBTK/TBTKMacros.h"
+
+using namespace std;
+
+namespace TBTK{
+namespace Property{
+
+/*Susceptibility::Susceptibility() : EnergyResolvedProperty(){
+}*/
+
+InteractionVertex::InteractionVertex(
+	const IndexTree &indexTree,
+	double lowerBound,
+	double upperBound,
+	unsigned int resolution
+) :
+	EnergyResolvedProperty(indexTree, lowerBound, upperBound, resolution)
+{
+}
+
+InteractionVertex::InteractionVertex(
+	const IndexTree &indexTree,
+	double lowerBound,
+	double upperBound,
+	unsigned int resolution,
+	const complex<double> *data
+) :
+	EnergyResolvedProperty(indexTree, lowerBound, upperBound, resolution, data)
+{
+}
+
+InteractionVertex::InteractionVertex(
+	const IndexTree &indexTree,
+	int lowerMatsubaraEnergyIndex,
+	int upperMatsubaraEnergyIndex,
+	double fundamentalMatsubaraEnergy
+) :
+	EnergyResolvedProperty(
+		EnergyType::BosonicMatsubara,
+		indexTree,
+		lowerMatsubaraEnergyIndex,
+		upperMatsubaraEnergyIndex,
+		fundamentalMatsubaraEnergy
+	)
+{
+}
+
+InteractionVertex::InteractionVertex(
+	const IndexTree &indexTree,
+	int lowerMatsubaraEnergyIndex,
+	int upperMatsubaraEnergyIndex,
+	double fundamentalMatsubaraEnergy,
+	const complex<double> *data
+) :
+	EnergyResolvedProperty(
+		EnergyType::BosonicMatsubara,
+		indexTree,
+		lowerMatsubaraEnergyIndex,
+		upperMatsubaraEnergyIndex,
+		fundamentalMatsubaraEnergy,
+		data
+	)
+{
+}
+
+};	//End of namespace Property
+};	//End of namespace TBTK
