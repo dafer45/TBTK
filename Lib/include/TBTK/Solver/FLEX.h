0506b89a5c2c125d5845c87b72c3dbd09de8188b
Kristofer Björnson <dafer45@hotmail.com>
2018-08-29 17:02:59 +0200

    Began work on Solver::FLEX.

---
+++ Lib/include/TBTK/Solver/FLEX.h
@@ -0,0 +1,266 @@
+/* Copyright 2018 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @package TBTKcalc
+ *  @file FLEX.h
+ *  @brief Executes the fluctuation exchange (FLEX) loop.
+ *
+ *  @author Kristofer Björnson
+ */
+
+#ifndef COM_DAFER45_TBTK_SOLVER_FLEX
+#define COM_DAFER45_TBTK_SOLVER_FLEX
+
+#include "TBTK/Property/GreensFunction.h"
+#include "TBTK/Property/Susceptibility.h"
+#include "TBTK/Property/InteractionVertex.h"
+#include "TBTK/Property/SelfEnergy.h"
+#include "TBTK/RPA/MomentumSpaceContext.h"
+#include "TBTK/Solver/Solver.h"
+
+namespace TBTK{
+namespace Solver{
+
+class FLEX : public Solver{
+public:
+	/** Enum class for specifying the current state of the solver. */
+	enum class State {
+		NotYetStarted,
+		GreensFunctionCalculated,
+		BareSusceptibilityCalculated,
+		RPASusceptibilitiesCalculated,
+		InteractionVertexCalculated,
+		SelfEnergyCalculated
+	};
+
+	/** Constructor. */
+	FLEX(const MomentumSpaceContext &momentumSpaceContext);
+
+	/** Get momentum cpsace context. */
+	const MomentumSpaceContext& getMomentumSpaceContext() const;
+
+	/** Get the GreensFunction.
+	 *
+	 *  @return The GreensFunction. */
+	const Property::GreensFunction& getGreensFunction() const;
+
+	/** Get the bare Susceptibility.
+	 *
+	 *  @return The bare Susceptibility. */
+	const Property::Susceptibility& getBareSusceptibility() const;
+
+	/** Get the RPA spin Susceptibility.
+	 *
+	 *  @return The RPA spin Susceptibility. */
+	const Property::Susceptibility& getRPASpinSusceptibility() const;
+
+	/** Get the RPA charge Susceptibility.
+	 *
+	 *  @return The RPA charge Susceptibility. */
+	const Property::Susceptibility& getRPAChargeSusceptibility() const;
+
+	/** Get the InteractionVertex.
+	 *
+	 *  @return The InteractionVertex. */
+	const Property::InteractionVertex& getInteractionVertex() const;
+
+	/** Get the SelfEnergy.
+	 *
+	 *  @return The SelfEnergy. */
+	const Property::SelfEnergy& getSelfEnergy() const;
+
+	/** Set the energy window used for the calculation.
+	 *
+	 *  @param lowerFermionicMatsubaraEnergyIndex The lower Fermionic
+	 *  Matsubara energy index used for quantities with Fermionic Matsubara
+	 *  energies.
+	 *
+	 *  @param upperFermionicMatsubaraEnergyIndex The upper Fermionic
+	 *  Matsubara energy index used for quantities with Fermionic Matsubara
+	 *  energies.
+	 *
+	 *  @param lowerBosonicMatsubaraEnergyIndex The lower Bosonic Matsubara
+	 *  energy index used for quantities with Bosonic Matsubara energies.
+	 *
+	 *  @param upperBosonicMatsubaraEnergyIndex The upper Bosonic Matsubara
+	 *  energy index used for quantities with Bosonic Matsubara energies. */
+	void setEnergyWindow(
+		int lowerFermionicMatsubaraEnergyIndex,
+		int upperFermionicMatsubaraEnergyIndex,
+		int lowerBosonicMatsubaraEnergyIndex,
+		int upperBosonicMatsubaraEnergyIndex
+	);
+
+	/** Set the interaction term U.
+	 *
+	 *  @param U The value of U. */
+	void setU(double U);
+
+	/** Set the interaction term J.
+	 *
+	 *  @param U The value of J. */
+	void setJ(double J);
+
+	/** Get the current state.
+	 *
+	 *  @return The current state of the solver. */
+	State getState() const;
+
+	/** Set callback to be called at each step in the loop.
+	 *
+	 *  @param callback The callback. Set to nullptr to disable callbacks.
+	 */
+	void setCallback(void (*callback)(FLEX &solver));
+
+	/** Execute the FLEX loop. */
+	void run();
+private:
+	/** Momentum space context. */
+	const MomentumSpaceContext &momentumSpaceContext;
+
+	/** Green's function. */
+	Property::GreensFunction greensFunction;
+
+	/** Susceptibility. */
+	Property::Susceptibility bareSusceptibility;
+
+	/** RPA spin Susceptibility. */
+	Property::Susceptibility rpaSpinSusceptibility;
+
+	/** RPA charge Susceptibility. */
+	Property::Susceptibility rpaChargeSusceptibility;
+
+	/** Interaction vertex. */
+	Property::InteractionVertex interactionVertex;
+
+	/** Interaction vertex. */
+	Property::SelfEnergy selfEnergy;
+
+	/** The lower Fermionic Matsubara energy index. */
+	int lowerFermionicMatsubaraEnergyIndex;
+
+	/** The upper Fermionic Matsubara energy index. */
+	int upperFermionicMatsubaraEnergyIndex;
+
+	/** The lower Bosonic Matsubara energy index. */
+	int lowerBosonicMatsubaraEnergyIndex;
+
+	/** The upp Bosonic Matsubara energy index. */
+	int upperBosonicMatsubaraEnergyIndex;
+
+	/** The interaction term U. */
+	double U;
+
+	/** The interaction term J. */
+	double J;
+
+	/** The current state the solver is in. */
+	State state;
+
+	/** Callback that is called after each step in the loop. */
+	void (*callback)(FLEX &solver);
+
+	/** Convert the self-energy index structure from
+	 *  {{block}, {intra block 0}, {intra block 1}} to
+	 *  {{block, intra block 0}, {block, intra block 1}}*/
+	void convertSelfEnergyIndexStructure();
+};
+
+inline const MomentumSpaceContext& FLEX::getMomentumSpaceContext() const{
+	return momentumSpaceContext;
+}
+
+inline const Property::GreensFunction& FLEX::getGreensFunction() const{
+	return greensFunction;
+}
+
+inline const Property::Susceptibility& FLEX::getBareSusceptibility() const{
+	return bareSusceptibility;
+}
+
+inline const Property::Susceptibility& FLEX::getRPASpinSusceptibility() const{
+	return rpaSpinSusceptibility;
+}
+
+inline const Property::Susceptibility& FLEX::getRPAChargeSusceptibility(
+) const{
+	return rpaChargeSusceptibility;
+}
+
+inline const Property::InteractionVertex& FLEX::getInteractionVertex() const{
+	return interactionVertex;
+}
+
+inline const Property::SelfEnergy& FLEX::getSelfEnergy() const{
+	return selfEnergy;
+}
+
+inline void FLEX::setEnergyWindow(
+	int lowerFermionicMatsubaraEnergyIndex,
+	int upperFermionicMatsubaraEnergyIndex,
+	int lowerBosonicMatsubaraEnergyIndex,
+	int upperBosonicMatsubaraEnergyIndex
+){
+	TBTKAssert(
+		lowerFermionicMatsubaraEnergyIndex
+			<= upperFermionicMatsubaraEnergyIndex,
+		"Solver::FLEX::setEnergyWindow()",
+		"The 'lowerFermionicMatsubaraEnergyIndex="
+		<< lowerFermionicMatsubaraEnergyIndex << "' must be less or"
+		<< " equal to the 'upperFermionicMatsubaraEnergyIndex="
+		<< upperFermionicMatsubaraEnergyIndex << "'.",
+		""
+	);
+	TBTKAssert(
+		lowerBosonicMatsubaraEnergyIndex
+			<= upperBosonicMatsubaraEnergyIndex,
+		"Solver::FLEX::setEnergyWindow()",
+		"The 'lowerBosonicMatsubaraEnergyIndex="
+		<< lowerBosonicMatsubaraEnergyIndex << "' must be less or"
+		<< " equal to the 'upperBosonicMatsubaraEnergyIndex="
+		<< upperBosonicMatsubaraEnergyIndex << "'.",
+		""
+	);
+
+	this->lowerFermionicMatsubaraEnergyIndex
+		= lowerFermionicMatsubaraEnergyIndex;
+	this->upperFermionicMatsubaraEnergyIndex
+		= upperFermionicMatsubaraEnergyIndex;
+	this->lowerBosonicMatsubaraEnergyIndex
+		= lowerBosonicMatsubaraEnergyIndex;
+	this->upperBosonicMatsubaraEnergyIndex
+		= upperBosonicMatsubaraEnergyIndex;
+}
+
+inline void FLEX::setU(double U){
+	this->U = U;
+}
+
+inline void FLEX::setJ(double J){
+	this->J = J;
+}
+
+inline FLEX::State FLEX::getState() const{
+	return state;
+}
+
+inline void FLEX::setCallback(void (*callback)(FLEX &solver)){
+	this->callback = callback;
+}
+
+};	//End of namespace Solver
+};	//End of namespace TBTK
+
+#endif
---
+++ Lib/src/Solver/FLEX.cpp
@@ -0,0 +1,345 @@
+/* Copyright 2018 Kristofer Björnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @file FLEX.cpp
+ *
+ *  @author Kristofer Björnson
+ */
+
+#include "TBTK/PropertyExtractor/BlockDiagonalizer.h"
+#include "TBTK/PropertyExtractor/ElectronFluctuationVertex.h"
+#include "TBTK/PropertyExtractor/MatsubaraSusceptibility.h"
+#include "TBTK/PropertyExtractor/RPASusceptibility.h"
+#include "TBTK/PropertyExtractor/SelfEnergy2.h"
+#include "TBTK/Solver/BlockDiagonalizer.h"
+#include "TBTK/Solver/ElectronFluctuationVertex.h"
+#include "TBTK/Solver/MatsubaraSusceptibility.h"
+#include "TBTK/Solver/RPASusceptibility.h"
+#include "TBTK/Solver/SelfEnergy2.h"
+#include "TBTK/Solver/FLEX.h"
+
+#include <complex>
+
+using namespace std;
+
+namespace TBTK{
+namespace Solver{
+
+FLEX::FLEX(const MomentumSpaceContext &momentumSpaceContext) :
+	momentumSpaceContext(momentumSpaceContext)
+{
+	lowerFermionicMatsubaraEnergyIndex = -1;
+	upperFermionicMatsubaraEnergyIndex = 1;
+	lowerBosonicMatsubaraEnergyIndex = 0;
+	upperBosonicMatsubaraEnergyIndex = 0;
+
+	U = 0;
+	J = 0;
+
+	state = State::NotYetStarted;
+	callback = nullptr;
+}
+
+void FLEX::run(){
+	const vector<unsigned int> &numMeshPoints
+		= momentumSpaceContext.getNumMeshPoints();
+	TBTKAssert(
+		numMeshPoints.size() == 2,
+		"Solver::FLEX::run()",
+		"Only two-dimensional block indices supported yet, but the"
+		<< " MomentumSpaceContext has a '" << numMeshPoints.size()
+		<< "'-dimensional block structure.",
+		""
+	);
+
+	BlockDiagonalizer blockDiagonalizer;
+	blockDiagonalizer.setModel(getModel());
+	blockDiagonalizer.run();
+
+	//Calculate the non-interacting Green's function.
+	Timer::tick("Green's function 0");
+	vector<Index> greensFunctionPatterns;
+	for(int kx = 0; kx < (int)numMeshPoints[0]; kx++){
+		for(int ky = 0; ky < (int)numMeshPoints[1]; ky++){
+			greensFunctionPatterns.push_back(
+				{{kx, ky, IDX_ALL}, {kx, ky, IDX_ALL}}
+			);
+		}
+	}
+
+	PropertyExtractor::BlockDiagonalizer
+		blockDiagonalizerPropertyExtractor(blockDiagonalizer);
+	blockDiagonalizerPropertyExtractor.setEnergyWindow(
+		lowerFermionicMatsubaraEnergyIndex,
+		upperFermionicMatsubaraEnergyIndex,
+		lowerBosonicMatsubaraEnergyIndex,
+		upperBosonicMatsubaraEnergyIndex
+	);
+	greensFunction
+		= blockDiagonalizerPropertyExtractor.calculateGreensFunction(
+			greensFunctionPatterns,
+			Property::GreensFunction::Type::Matsubara
+		);
+	Timer::tock();
+
+	state = State::GreensFunctionCalculated;
+	if(callback != nullptr)
+		callback(*this);
+
+	//The main loop.
+	bool done = false;
+	while(!done){
+		//Calculate the bare susceptibility.
+		Timer::tick("Bare susceptibility");
+		MatsubaraSusceptibility matsubaraSusceptibilitySolver(
+			momentumSpaceContext,
+			greensFunction
+		);
+		matsubaraSusceptibilitySolver.setModel(getModel());
+
+		PropertyExtractor::MatsubaraSusceptibility
+			matsubaraSusceptibilityPropertyExtractor(
+				matsubaraSusceptibilitySolver
+			);
+		matsubaraSusceptibilityPropertyExtractor.setEnergyWindow(
+			lowerFermionicMatsubaraEnergyIndex,
+			upperFermionicMatsubaraEnergyIndex,
+			lowerBosonicMatsubaraEnergyIndex,
+			upperBosonicMatsubaraEnergyIndex
+		);
+		Streams::out
+			<< lowerFermionicMatsubaraEnergyIndex << "\t"
+			<< upperFermionicMatsubaraEnergyIndex << "\t"
+			<< lowerBosonicMatsubaraEnergyIndex << "\t"
+			<< upperBosonicMatsubaraEnergyIndex << "\n";
+		bareSusceptibility
+			= matsubaraSusceptibilityPropertyExtractor.calculateSusceptibility({
+				{
+					{IDX_ALL, IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL}
+				}
+			});
+		Timer::tock();
+
+		state = State::BareSusceptibilityCalculated;
+		if(callback != nullptr)
+			callback(*this);
+
+		//Calculate the RPA charge and spin susceptibilities.
+		Timer::tick("RPA susceptibilities");
+		RPASusceptibility rpaSusceptibilitySolver(
+			momentumSpaceContext,
+			bareSusceptibility
+		);
+		rpaSusceptibilitySolver.setModel(getModel());
+		rpaSusceptibilitySolver.setU(U);
+		rpaSusceptibilitySolver.setJ(J);
+		rpaSusceptibilitySolver.setUp(U - 2.*J);
+		rpaSusceptibilitySolver.setJp(J);
+
+		PropertyExtractor::RPASusceptibility
+			rpaSusceptibilityPropertyExtractor(
+				rpaSusceptibilitySolver
+			);
+		rpaChargeSusceptibility
+			= rpaSusceptibilityPropertyExtractor.calculateChargeSusceptibility({
+				{
+					{IDX_ALL, IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL}
+				}
+			});
+		rpaSpinSusceptibility
+			= rpaSusceptibilityPropertyExtractor.calculateSpinSusceptibility({
+				{
+					{IDX_ALL, IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL}
+				}
+			});
+		Timer::tock();
+
+		state = State::RPASusceptibilitiesCalculated;
+		if(callback != nullptr)
+			callback(*this);
+
+		//Calculate the interaction vertex.
+		Timer::tick("Interaction vertex");
+		ElectronFluctuationVertex
+			electronFluctuationVertexSolver(
+				momentumSpaceContext,
+				rpaChargeSusceptibility,
+				rpaSpinSusceptibility
+			);
+		electronFluctuationVertexSolver.setModel(getModel());
+		electronFluctuationVertexSolver.setU(U);
+		electronFluctuationVertexSolver.setJ(J);
+		electronFluctuationVertexSolver.setUp(U - 2.*J);
+		electronFluctuationVertexSolver.setJp(J);
+
+		PropertyExtractor::ElectronFluctuationVertex
+			electronFluctuationVertexPropertyExtractor(
+				electronFluctuationVertexSolver
+			);
+		interactionVertex
+			= electronFluctuationVertexPropertyExtractor.calculateInteractionVertex({
+				{
+					{IDX_ALL, IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL},
+					{IDX_ALL}
+				}
+			});
+		Timer::tock();
+
+		state = State::InteractionVertexCalculated;
+		if(callback != nullptr)
+			callback(*this);
+
+		//Calculate the self-energy.
+		Timer::tick("Self-energy");
+		SelfEnergy2 selfEnergySolver(
+			momentumSpaceContext,
+			interactionVertex,
+			greensFunction
+		);
+		selfEnergySolver.setModel(getModel());
+
+		PropertyExtractor::SelfEnergy2 selfEnergyPropertyExtractor(
+			selfEnergySolver
+		);
+		selfEnergyPropertyExtractor.setEnergyWindow(
+			lowerFermionicMatsubaraEnergyIndex,
+			upperFermionicMatsubaraEnergyIndex,
+			lowerBosonicMatsubaraEnergyIndex,
+			upperBosonicMatsubaraEnergyIndex
+		);
+		selfEnergy
+			= selfEnergyPropertyExtractor.calculateSelfEnergy({
+				{{IDX_ALL, IDX_ALL}, {IDX_ALL}, {IDX_ALL}}
+			});
+		convertSelfEnergyIndexStructure();
+		Timer::tock();
+
+		state = State::SelfEnergyCalculated;
+		if(callback != nullptr)
+			callback(*this);
+
+		done = true;
+	}
+}
+
+void FLEX::convertSelfEnergyIndexStructure(){
+	const vector<unsigned int> &numMeshPoints
+		= momentumSpaceContext.getNumMeshPoints();
+	TBTKAssert(
+		numMeshPoints.size() == 2,
+		"Solver::FLEX::convertSelfEnergyBlockStructure()",
+		"Only two-dimensional block indices supported yet, but the"
+		<< " MomentumSpaceContext has a '" << numMeshPoints.size()
+		<< "'-dimensional block structure.",
+		""
+	);
+	unsigned int numOrbitals = momentumSpaceContext.getNumOrbitals();
+
+	IndexTree memoryLayout;
+	for(unsigned int kx = 0; kx < numMeshPoints[0]; kx++){
+		for(unsigned int ky = 0; ky < numMeshPoints[1]; ky++){
+			for(
+				unsigned int orbital0 = 0;
+				orbital0 < numOrbitals;
+				orbital0++
+			){
+				for(
+					unsigned int orbital1 = 0;
+					orbital1 < numOrbitals;
+					orbital1++
+				){
+					memoryLayout.add({
+						{(int)kx, (int)ky, (int)orbital0},
+						{(int)kx, (int)ky, (int)orbital1}
+					});
+				}
+			}
+		}
+	}
+	memoryLayout.generateLinearMap();
+	Property::SelfEnergy newSelfEnergy(
+		memoryLayout,
+		selfEnergy.getLowerMatsubaraEnergyIndex(),
+		selfEnergy.getUpperMatsubaraEnergyIndex(),
+		selfEnergy.getFundamentalMatsubaraEnergy()
+	);
+	for(unsigned int kx = 0; kx < numMeshPoints[0]; kx++){
+		for(unsigned int ky = 0; ky < numMeshPoints[1]; ky++){
+			for(
+				unsigned int orbital0 = 0;
+				orbital0 < numOrbitals;
+				orbital0++
+			){
+				for(
+					unsigned int orbital1 = 0;
+					orbital1 < numOrbitals;
+					orbital1++
+				){
+					for(
+						unsigned int n = 0;
+						n < selfEnergy.getNumMatsubaraEnergies();
+						n++
+					){
+						newSelfEnergy(
+							{
+								{
+									(int)kx,
+									(int)ky,
+									(int)orbital0
+								},
+								{
+									(int)kx,
+									(int)ky,
+									(int)orbital1
+								}
+							},
+							n
+						) = selfEnergy(
+							{
+								{
+									(int)kx,
+									(int)ky
+								},
+								{(int)orbital0},
+								{(int)orbital1}
+							},
+							n
+						);
+					}
+				}
+			}
+		}
+	}
+
+	selfEnergy = newSelfEnergy;
+}
+
+}	//End of namespace Solver
+}	//End of namesapce TBTK
