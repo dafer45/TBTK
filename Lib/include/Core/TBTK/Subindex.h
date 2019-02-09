90deb06cc8b276ab8dbcdcb995704d246d7c3710
Kristofer Björnson <dafer45@hotmail.com>
2019-02-09 22:27:33 +0100

    Added more constructors and operators to Subindex.

--- Lib/include/Core/TBTK/Subindex.h
+++ Lib/include/Core/TBTK/Subindex.h
@@ -39,6 +39,11 @@ public:
 	 *  @param value The value to initilize the Subindex with. */
 	constexpr Subindex(Integer value) : value(value) {}
 
+	/** Constructor.
+	 *
+	 *  @param value The value to initilize the Subindex with. */
+	constexpr Subindex(int value) : value(value) {}
+
 	/** Constructs a Subindex from a serialization string.
 	 *
 	 *  @param serialization Serialization string from which to construct
@@ -46,6 +51,9 @@ public:
 	 *
 	 *  @param mode Mode with which the string has been serialized. */
 	Subindex(const std::string &serialization, Serializable::Mode mode);
+
+	/** Type conversion operator. */
+	constexpr explicit operator int() const{ return value;	};
 
 	/** Assignment operator.
 	 *
@@ -58,6 +66,17 @@ public:
 		return *this;
 	}
 
+	/** Assignment operator.
+	 *
+	 *  @param value The value to assign the Subindex.
+	 *
+	 *  @return The Subindex after assignment has occured. */
+	Subindex& operator=(int rhs){
+		value = rhs;
+
+		return *this;
+	}
+
 	/** Comparison operator.
 	 *
 	 *  @param rhs The right hand side.
@@ -65,6 +84,47 @@ public:
 	 *  @return True if the two subindices agree, otherwise false. */
 	bool operator==(Subindex rhs) const{
 		return value == rhs.value;
+	}
+
+	/** Not equal operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return False if the two subindices agree, otherwise true. */
+	bool operator!=(Subindex rhs) const{
+		return !operator==(rhs);
+	}
+
+	/** Less than operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return True if the left hand side is smaller than the right hand
+	 *  side, otherwise false. */
+	bool operator<(Subindex rhs) const{
+		return value < rhs.value;
+	}
+
+	/** Larger than operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return True if the left hand side is larger than the right hand
+	 *  side, otherwise false */
+	bool operator>(Subindex rhs) const{
+		return value > rhs.value;
+	}
+
+	/** ostream operator.
+	 *
+	 *  @param os The ostream to write to.
+	 *  @param rhs The Subindex to write.
+	 *
+	 *  @return The ostream. */
+	friend std::ostream& operator<<(std::ostream &os, const Subindex subindex){
+		os << subindex.value;
+
+		return os;
 	}
 
 	/** Serialize Subindex. Note that Subindex is PseudoSerializable rather
--- Test/include/Core/TBTK/Test/Subindex.h
+++ Test/include/Core/TBTK/Test/Subindex.h
@@ -15,7 +15,7 @@ TEST(Subindex, Constructor){
 	EXPECT_EQ(subindex0, subindex2);
 }
 
-TEST(Integer, serializeToJSON){
+TEST(Subindex, serializeToJSON){
 	Subindex subindex0(7);
 	Subindex subindex1(
 		subindex0.serialize(Serializable::Mode::JSON),
@@ -25,15 +25,25 @@ TEST(Integer, serializeToJSON){
 	EXPECT_EQ(subindex0, subindex1);
 }
 
+TEST(Subindex, operatorInt){
+	Subindex subindex(7);
+	int i = (int)subindex;
+
+	EXPECT_EQ(i, 7);
+}
+
 TEST(Subindex, operatorAssignment){
 	Subindex subindex0(7);
 	Subindex subindex1(0);
 	Subindex subindex2(0);
-	subindex1 = (int)7;
-	subindex2 = (unsigned int)7;
+	Subindex subindex3(0);
+	subindex1 = Integer(7);
+	subindex2 = (int)7;
+	subindex3 = (unsigned int)7;
 
 	EXPECT_EQ(subindex0, subindex1);
 	EXPECT_EQ(subindex0, subindex2);
+	EXPECT_EQ(subindex0, subindex3);
 }
 
 TEST(Subindex, operatorComparsion){
@@ -43,6 +53,35 @@ TEST(Subindex, operatorComparsion){
 
 	EXPECT_EQ(subindex0, subindex1);
 	EXPECT_FALSE(subindex0 == subindex2);
+}
+
+TEST(Subindex, operatorNotEqual){
+	Subindex subindex0(7);
+	Subindex subindex1(7);
+	Subindex subindex2(8);
+
+	EXPECT_FALSE(subindex0 != subindex1);
+	EXPECT_TRUE(subindex0 != subindex2);
+}
+
+TEST(Subindex, operatorLessThan){
+	Subindex subindex(7);
+
+	EXPECT_TRUE(7 < 8);
+	EXPECT_FALSE(7 < 7);
+	EXPECT_FALSE(7 < 6);
+}
+
+TEST(Subindex, operatorLargerThan){
+	Subindex subindex(7);
+
+	EXPECT_TRUE(7 > 6);
+	EXPECT_FALSE(7 > 7);
+	EXPECT_FALSE(7 > 8);
+}
+
+TEST(Subindex , operatorOstream){
+	//Not testable.
 }
 
 #endif
