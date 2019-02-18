a8114c875c5d9aee54f5051c25a55b73f8df7fc2
Kristofer Björnson <dafer45@hotmail.com>
2019-02-18 15:03:10 +0100

    Added functions and constructors to Complex to make it more flexible in combination with other types.

--- Lib/include/Core/TBTK/Complex.h
+++ Lib/include/Core/TBTK/Complex.h
@@ -23,7 +23,9 @@
 #ifndef COM_DAFER45_TBTK_COMPLEX
 #define COM_DAFER45_TBTK_COMPLEX
 
+#include "TBTK/Boolean.h"
 #include "TBTK/PseudoSerializable.h"
+#include "TBTK/Real.h"
 
 #include <complex>
 #include <sstream>
@@ -42,6 +44,16 @@ public:
 	 *
 	 *  @param value The value to initilize the Complex with. */
 	constexpr Complex(std::complex<double> value) : value(value) {}
+
+	/** Constructor.
+	 *
+	 *  @param value The value to initilize the Complex with. */
+	constexpr Complex(Real real, Real imag) : value(real, imag) {}
+
+	/** Constructor.
+	 *
+	 *  @param value The value to initilize the Complex with. */
+	constexpr Complex(double value) : value(value) {}
 
 	/** Constructs an Index from a serialization string.
 	 *
@@ -78,6 +90,47 @@ public:
 		return *this;
 	}
 
+	/** Addition assignment operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The left hand side after the addition has occured. */
+	friend std::complex<double>& operator+=(
+		std::complex<double> &lhs,
+		const Complex &rhs
+	){
+		lhs += rhs.value;
+
+		return lhs;
+	}
+
+	/** Addition operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the addition. */
+	friend Complex operator+(const Complex &lhs, const double &rhs){
+		Complex result(lhs);
+		result.value += rhs;
+
+		return result;
+	}
+
+	/** Addition operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the addition. */
+	friend Complex operator+(const double &lhs, const Complex &rhs){
+		Complex result(rhs);
+		result.value += lhs;
+
+		return result;
+	}
+
 	/** Subtraction assignment operator.
 	 *
 	 *  @param rhs The right hand side.
@@ -87,6 +140,50 @@ public:
 		value -= rhs.value;
 
 		return *this;
+	}
+
+	/** Subtraction operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the subtraction. */
+	Complex operator-(const Complex &rhs) const{
+		Complex result(*this);
+		result -= rhs;
+
+		return result;
+	}
+
+	/** Subtraction operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the subtraction. */
+	friend Complex operator-(const Complex &lhs, const double &rhs){
+		Complex result(lhs);
+		result.value -= rhs;
+
+		return result;
+	}
+
+	/** Subtraction operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the subtraction. */
+	friend Complex operator-(const double &lhs, const Complex &rhs){
+		Complex result(lhs - rhs.value.real(), -rhs.value.imag());
+
+		return result;
+	}
+
+	/** Unary minus operator.
+	 *
+	 *  @return The negative of the Complex number. */
+	Complex operator-() const{
+		return Complex(-value);
 	}
 
 	/** Multiplication assignment operator.
@@ -100,6 +197,32 @@ public:
 		return *this;
 	}
 
+	/** Multiplication operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the multiplication. */
+	friend Complex operator*(const Complex &lhs, const double &rhs){
+		Complex result(lhs);
+		result.value *= rhs;
+
+		return result;
+	}
+
+	/** Multiplication operator.
+	 *
+	 *  @param lhs The left hand side.
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the multiplication. */
+	friend Complex operator*(const double &lhs, const Complex &rhs){
+		Complex result(rhs);
+		result.value *= lhs;
+
+		return result;
+	}
+
 	/** Division assignment operator.
 	 *
 	 *  @param rhs The right hand side.
@@ -111,6 +234,60 @@ public:
 		return *this;
 	}
 
+	/** Division operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return The result of the division. */
+	friend Complex operator/(const Complex &lhs, const double &rhs){
+		Complex result(lhs);
+		result.value /= rhs;
+
+		return result;
+	}
+
+	/** Comparison operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return True if the two Complex numbers are equal, otherwise false. */
+	Boolean operator==(const Complex &rhs) const{
+		return value == rhs.value;
+	}
+
+	/** Not equal operator.
+	 *
+	 *  @param rhs The right hand side.
+	 *
+	 *  @return False if the two Complex numbers are equal, otherwise true. */
+	Boolean operator!=(const Complex &rhs) const{
+		return value != rhs.value;
+	}
+
+	/** ostream operator.
+	 *
+	 *  @param os The ostream to write to.
+	 *  @param complex The Complex to write.
+	 *
+	 *  @return The ostream. */
+	friend std::ostream& operator<<(std::ostream &os, const Complex &complex){
+		os << complex.value;
+
+		return os;
+	}
+
+	/** istream operator.
+	 *
+	 *  @param is The istream to read from.
+	 *  @param complex The Complex to read to.
+	 *
+	 *  @return The istream. */
+	friend std::istream& operator>>(std::istream &is, Complex &complex){
+		is >> complex.value;
+
+		return is;
+	}
+
 	/** Serialize Complex. Note that Complex is PseudoSerializable rather
 	 *  than Serializable. This means that the Serializable interface is
 	 *  implemented non-virtually.
@@ -119,6 +296,42 @@ public:
 	 *
 	 *  @return Serialized string representation of the Complex. */
 	std::string serialize(Serializable::Mode mode) const;
+
+	/** Get the real part.
+	 *
+	 *  @param complex The Complex number.
+	 *
+	 *  @return The real part. */
+	friend constexpr Real real(const Complex &complex){
+		return real(complex.value);
+	}
+
+	/** Get the imaginary part.
+	 *
+	 *  @param complex The Complex number.
+	 *
+	 *  @return The imaginary part. */
+	friend constexpr Real imag(const Complex &complex){
+		return imag(complex.value);
+	}
+
+	/** Complex conjugate.
+	 *
+	 *  @param complex The Complex number.
+	 *
+	 *  @return The complex conjugate. */
+	friend Complex conj(const Complex &complex){
+		return conj(complex.value);
+	}
+
+	/** Absolute value.
+	 *
+	 *  @param complex The Complex number.
+	 *
+	 *  @return The absolute value. */
+	friend Real abs(const Complex &complex){
+		return abs(complex.value);
+	}
 private:
 	/** Value. */
 	std::complex<double> value;
--- Test/include/Core/TBTK/Test/Complex.h
+++ Test/include/Core/TBTK/Test/Complex.h
@@ -18,6 +18,21 @@ TEST(Complex, Constructor1){
 	EXPECT_EQ(std::imag(c), 3);
 }
 
+TEST(Complex, Constructor2){
+	Complex complex(7, 3);
+
+	EXPECT_EQ(real(complex), 7);
+	EXPECT_EQ(imag(complex), 3);
+}
+
+TEST(Complex, Constructor3){
+	Complex complex(7);
+
+	std::complex<double> c = complex;
+	EXPECT_EQ(std::real(c), 7);
+	EXPECT_EQ(std::imag(c), 0);
+}
+
 TEST(Complex, serializeToJSON){
 	Complex complex0(std::complex<double>(7, 3));
 	Complex complex1(
@@ -47,13 +62,34 @@ TEST(Complex, operatorAssignment){
 	EXPECT_EQ(std::imag(c), 3);
 }
 
-TEST(Complex, operatorAdditionAssignment){
+TEST(Complex, operatorAdditionAssignment0){
 	Complex complex(7);
 	complex += std::complex<double>(0, 3);
 
 	std::complex<double> c = complex;
 	EXPECT_EQ(std::real(c), 7);
 	EXPECT_EQ(std::imag(c), 3);
+}
+
+TEST(Complex, operatorAdditionAssignment1){
+	std::complex<double> complex(7, 0);
+	complex += Complex(0, 3);
+
+	std::complex<double> c = complex;
+	EXPECT_EQ(std::real(c), 7);
+	EXPECT_EQ(std::imag(c), 3);
+}
+
+TEST(Complex, operatorAddition0){
+	Complex complex(0, 3);
+
+	EXPECT_EQ(complex + 7, Complex(7, 3));
+}
+
+TEST(Complex, operatorAddition1){
+	Complex complex(0, 3);
+
+	EXPECT_EQ(7 + complex, Complex(7, 3));
 }
 
 TEST(Complex, operatorSubtractionAssignment){
@@ -65,6 +101,22 @@ TEST(Complex, operatorSubtractionAssignment){
 	EXPECT_EQ(std::imag(c), -3);
 }
 
+TEST(Complex, operatorSubtraction0){
+	EXPECT_EQ(Complex(7, 0) - Complex(0, 3), Complex(7, -3));
+}
+
+TEST(Complex, operatorSubtraction1){
+	EXPECT_EQ(Complex(0, 3) - 7, Complex(-7, 3));
+}
+
+TEST(Complex, operatorSubtraction2){
+	EXPECT_EQ(7 - Complex(0, 3), Complex(7, -3));
+}
+
+TEST(Complex, operatorUnaryMinus){
+	EXPECT_EQ(-Complex(7, 3), Complex(-7, -3));
+}
+
 TEST(Complex, operatorMultiplicationAssignment){
 	Complex complex(7);
 	complex *= std::complex<double>(0, 3);
@@ -74,6 +126,14 @@ TEST(Complex, operatorMultiplicationAssignment){
 	EXPECT_EQ(std::imag(c), 21);
 }
 
+TEST(Complex, operatorMultiplication0){
+	EXPECT_EQ(Complex(7, 3)*2, Complex(14, 6));
+}
+
+TEST(Complex, operatorMultiplication1){
+	EXPECT_EQ(2*Complex(7, 3), Complex(14, 6));
+}
+
 TEST(Complex, operatorDivisionAssignment){
 	Complex complex(6);
 	complex /= std::complex<double>(0, 3);
@@ -81,6 +141,54 @@ TEST(Complex, operatorDivisionAssignment){
 	std::complex<double> c = complex;
 	EXPECT_EQ(std::real(c), 0);
 	EXPECT_EQ(std::imag(c), -2);
+}
+
+TEST(Complex, operatorDivision0){
+	EXPECT_EQ(Complex(14, 6)/2, Complex(7, 3));
+}
+
+TEST(Complex, operatorComparison){
+	EXPECT_TRUE(Complex(7, 3) == Complex(7, 3));
+	EXPECT_FALSE(Complex(7, 3) == Complex(7, 0));
+	EXPECT_FALSE(Complex(7, 3) == Complex(0, 3));
+}
+
+TEST(Complex, operatorNotEqual){
+	EXPECT_FALSE(Complex(7, 3) != Complex(7, 3));
+	EXPECT_TRUE(Complex(7, 3) != Complex(7, 0));
+	EXPECT_TRUE(Complex(7, 3) != Complex(0, 3));
+}
+
+TEST(Complex, operatorOstream){
+	Complex complex(7, 3);
+	std::stringstream ss;
+	ss << complex;
+
+	EXPECT_TRUE(ss.str().compare("(7,3)") == 0);
+}
+
+TEST(Complex, operatorIstream){
+	Complex complex;
+	std::stringstream ss("(7,3)");
+	ss >> complex;
+
+	EXPECT_EQ(complex, Complex(7, 3));
+}
+
+TEST(Complex, real){
+	EXPECT_EQ(real(Complex(7, 3)), 7);
+}
+
+TEST(Complex, imag){
+	EXPECT_EQ(imag(Complex(7, 3)), 3);
+}
+
+TEST(Complex, conj){
+	EXPECT_EQ(conj(Complex(7, 3)), Complex(7, -3));
+}
+
+TEST(Complex, abs){
+	EXPECT_EQ(abs(Complex(3, 4)), 5);
 }
 
 #endif
