e44188ee902906e51623b37fcb6ed479bf03034e
Kristofer Björnson <dafer45@hotmail.com>
2019-04-09 21:26:05 +0200

    Corrected year in copyright notice.

--- Lib/include/StatesAndOperators/TBTK/Sto3g.h
+++ Lib/include/StatesAndOperators/TBTK/Sto3g.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 Kristofer Björnson
+/* Copyright 2019 Kristofer Björnson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
