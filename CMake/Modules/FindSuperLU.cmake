c9a01848343388908542f4148fd482d68bd2a5ef
Kristofer Björnson <dafer45@hotmail.com>
2019-10-07 22:58:12 +0200

    Updated FindSuperLU.cmake to avoid signaling that SuperLU is found if the found version is unable to compile TBTK code.

--- CMake/Modules/FindSuperLU.cmake
+++ CMake/Modules/FindSuperLU.cmake
@@ -11,10 +11,58 @@ FIND_LIBRARY(
 	PATH_SUFFIXES lib lib32 lib64 SRC
 )
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-	SuperLU FOUND_VAR SuperLU_FOUND
-	REQUIRED_VARS SUPER_LU_INCLUDES SUPER_LU_LIBRARIES
+SET(CMAKE_REQUIRED_INCLUDES slu_ddefs.h slu_zdefs.h)
+SET(CMAKE_REQUIRED_LIBRARIES superlu lapack blas)
+INCLUDE(CheckCXXSourceCompiles)
+CHECK_CXX_SOURCE_COMPILES(
+	"
+	#include <slu_ddefs.h>
+	#include <slu_zdefs.h>
+
+	int main(int argc, char **argv){
+		superlu_options_t options;
+		int *etree;
+		SuperMatrix matrixCP;
+		int panelSize;
+		int relax;
+		int lwork;
+		GlobalLU_t glu;
+		int info;
+
+		SuperMatrix *L;
+		SuperMatrix *U;
+		int *rowPermutations;
+		int *columnPermutations;
+		SuperLUStat_t *statistics;
+
+		zgstrf(
+                        &options,
+                        &matrixCP,
+                        relax,
+                        panelSize,
+                        etree,
+                        NULL,
+                        lwork,
+                        columnPermutations,
+                        rowPermutations,
+                        L,
+                        U,
+                        &glu,
+                        statistics,
+                        &info
+                );
+
+		return 0;
+	}"
+	SUPER_LU_COMPILED
 )
+
+IF(${SUPER_LU_COMPILED})
+	INCLUDE(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+		SuperLU FOUND_VAR SuperLU_FOUND
+		REQUIRED_VARS SUPER_LU_INCLUDES SUPER_LU_LIBRARIES
+	)
+ENDIF(${SUPER_LU_COMPILED})
 
 #MARK_AS_ADVANCED(SUPER_LU_INCLDUES SUPER_LU_LIBRARIES)
