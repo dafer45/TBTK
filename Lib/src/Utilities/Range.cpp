d698082842cfc34a057f96a93d855a280330adaf
Kristofer Björnson <dafer45@hotmail.com>
2019-11-26 20:23:03 +0100

    Removed incorrectly assigned authorship of Range.cpp.

--- Lib/src/Utilities/Range.cpp
+++ Lib/src/Utilities/Range.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2017 Kristofer Björnson and Andreas Theiler
+/* Copyright 2017 Kristofer Björnson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
