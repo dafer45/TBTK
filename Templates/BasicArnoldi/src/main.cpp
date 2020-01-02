8baceaea190db9df72c4d70348dba345b05f1536
Kristofer Björnson <dafer45@hotmail.com>
2020-01-02 23:16:01 +0100

    Updated Templates/BasicArnoldi.

--- Templates/BasicArnoldi/src/main.cpp
+++ Templates/BasicArnoldi/src/main.cpp
@@ -23,47 +23,55 @@
  *  @author Kristofer Björnson
  */
 
-#include "TBTK/FileWriter.h"
 #include "TBTK/Model.h"
 #include "TBTK/PropertyExtractor/ArnoldiIterator.h"
 #include "TBTK/Property/DOS.h"
 #include "TBTK/Property/EigenValues.h"
 #include "TBTK/Property/LDOS.h"
 #include "TBTK/Property/SpinPolarizedLDOS.h"
+#include "TBTK/Smooth.h"
 #include "TBTK/Solver/ArnoldiIterator.h"
+#include "TBTK/TBTK.h"
 #include "TBTK/Timer.h"
+#include "TBTK/Visualization/MatPlotLib/Plotter.h"
 
 #include <complex>
 
 using namespace std;
 using namespace TBTK;
+using namespace Visualization::MatPlotLib;
 
 const complex<double> i(0, 1);
 
 int main(int argc, char **argv){
-	Timer::tick("Full calculation");
-	//Lattice size
-	const int SIZE_X = 80;
-	const int SIZE_Y = 80;
+	//Initialize TBTK.
+	Initialize();
 
-	//Parameters
+	Timer::tick("Full calculation");
+
+	//Lattice size.
+	const int SIZE_X = 40;
+	const int SIZE_Y = 40;
+
+	//Parameters.
 	complex<double> mu = -1.0 + 0.0000001;
 	complex<double> t = 1.0;
 	complex<double> J = 0.25;
 
-	//Create model and set up hopping parameters
+	//Create model and set up hopping parameters.
 	Model model;
 	for(int x = 0; x < SIZE_X; x++){
 		for(int y = 0; y < SIZE_Y; y++){
 			for(int s = 0; s < 2; s++){
-				//Add hopping amplitudes corresponding to chemical potential
+				//Add hopping amplitudes corresponding to
+				//chemical potential.
 				model << HoppingAmplitude(
 					-mu + J*(1/2.-s),
 					{x,	y,	s},
 					{x,	y,	s}
 				);
 
-				//Add hopping parameters corresponding to t
+				//Add hopping parameters corresponding to t.
 				if(x+1 < SIZE_X){
 					model << HoppingAmplitude(
 						-t,
@@ -82,57 +90,89 @@ int main(int argc, char **argv){
 		}
 	}
 
-	//Construct model
+	//Construct model.
 	model.construct();
 
-	//Setup and run Solver::ArnoldiIterator
-	const int NUM_EIGEN_VALUES = 1600;
-	const int NUM_LANCZOS_VECTORS = 3200;
-	int MAX_ITERATIONS = 4000;
-	Solver::ArnoldiIterator aSolver;
-	aSolver.setMode(Solver::ArnoldiIterator::Mode::ShiftAndInvert);
-	aSolver.setModel(model);
-	aSolver.setCentralValue(1.0);
-	aSolver.setNumEigenValues(NUM_EIGEN_VALUES);
-	aSolver.setCalculateEigenVectors(true);
-	aSolver.setNumLanczosVectors(NUM_LANCZOS_VECTORS);
-	aSolver.setMaxIterations(MAX_ITERATIONS);
-	aSolver.run();
+	//Setup and run Solver::ArnoldiIterator.
+	const int NUM_EIGEN_VALUES = 400;
+	const int NUM_LANCZOS_VECTORS = 800;
+	int MAX_ITERATIONS = 2000;
+	Solver::ArnoldiIterator solver;
+	solver.setMode(Solver::ArnoldiIterator::Mode::ShiftAndInvert);
+	solver.setModel(model);
+	solver.setCentralValue(1.0);
+	solver.setNumEigenValues(NUM_EIGEN_VALUES);
+	solver.setCalculateEigenVectors(true);
+	solver.setNumLanczosVectors(NUM_LANCZOS_VECTORS);
+	solver.setMaxIterations(MAX_ITERATIONS);
+	solver.run();
 
-	//Set filename and remove any file already in the folder
-	FileWriter::setFileName("TBTKResults.h5");
-	FileWriter::clear();
+	//Create PropertyExtractor.
+	PropertyExtractor::ArnoldiIterator propertyExtractor(solver);
 
-	//Create PropertyExtractor
-	PropertyExtractor::ArnoldiIterator pe(aSolver);
-
-	//Setup energy window
+	//Setup energy window.
 	const double UPPER_BOUND = 1.25;
 	const double LOWER_BOUND = 0.75;
 	const int RESOLUTION = 1000;
-	pe.setEnergyWindow(LOWER_BOUND, UPPER_BOUND, RESOLUTION);
-
-	//Extract eigenvalues and write these to file
-	Property::EigenValues ev = pe.getEigenValues();
-	FileWriter::writeEigenValues(ev);
-
-	//Extract DOS and write to file
-	Property::DOS dos = pe.calculateDOS();
-	FileWriter::writeDOS(dos);
-
-	//Extract LDOS and write to file
-	Property::LDOS ldos = pe.calculateLDOS(
-		{IDX_X,		SIZE_Y/2,	IDX_SUM_ALL},
-		{SIZE_X,	1,		2}
+	propertyExtractor.setEnergyWindow(
+		LOWER_BOUND,
+		UPPER_BOUND,
+		RESOLUTION
 	);
-	FileWriter::writeLDOS(ldos);
 
-	//Extract spin-polarized LDOS and write to file
-	Property::SpinPolarizedLDOS spLdos = pe.calculateSpinPolarizedLDOS(
-		{IDX_X,		SIZE_Y/2,	IDX_SPIN},
-		{SIZE_X,	1,		2}
+	//Extract eigenvalues and write these to file.
+	Property::EigenValues eigenValues = propertyExtractor.getEigenValues();
+
+	//Plot the eigenvalues.
+	Plotter plotter;
+	plotter.plot(eigenValues);
+	plotter.save("figures/EigenValues.png");
+
+	//Extract the density of states (DOS).
+	Property::DOS dos = propertyExtractor.calculateDOS();
+
+	//Smooth the DOS.
+	const double SMOOTHING_SIGMA = 0.01;
+	const unsigned int SMOOTHING_WINDOW = 201;
+	dos = Smooth::gaussian(dos, SMOOTHING_SIGMA, SMOOTHING_WINDOW);
+
+	//Plot the DOS.
+	plotter.clear();
+	plotter.plot(dos);
+	plotter.save("figures/DOS.png");
+
+	//Extract the local density of states (LDOS).
+	Property::LDOS ldos = propertyExtractor.calculateLDOS({
+		{_a_, _a_, IDX_SUM_ALL}
+	});
+
+	//Smooth the LDOS.
+	ldos = Smooth::gaussian(ldos, SMOOTHING_SIGMA, SMOOTHING_WINDOW);
+
+	//Plot the LDOS.
+	plotter.clear();
+	plotter.setNumContours(100);
+	plotter.plot({_a_, SIZE_Y/2, IDX_SUM_ALL}, ldos);
+	plotter.save("figures/LDOS.png");
+
+	//Extract the spin-polarized LDOS.
+	Property::SpinPolarizedLDOS spinPolarizedLDOS
+		= propertyExtractor.calculateSpinPolarizedLDOS({
+			{_a_, _a_, IDX_SPIN}
+		});
+
+	//Smooth the spin-polarized LDOS.
+	spinPolarizedLDOS = Smooth::gaussian(
+		spinPolarizedLDOS,
+		SMOOTHING_SIGMA,
+		SMOOTHING_WINDOW
 	);
-	FileWriter::writeSpinPolarizedLDOS(spLdos);
+
+	//Plot the spin-polarized LDOS along the z-axis and for y=SIZE_Y/2.
+	plotter.clear();
+	plotter.setNumContours(100);
+	plotter.plot({_a_, SIZE_Y/2, IDX_SPIN}, {0, 0, 1}, spinPolarizedLDOS);
+	plotter.save("figures/SpinPolarizedLDOS.png");
 
 	Timer::tock();
 
