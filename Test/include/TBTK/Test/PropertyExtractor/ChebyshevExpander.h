cfc864d9ca418a64f107d907df47943b8a930a9e
Kristofer Björnson <dafer45@hotmail.com>
2021-06-16 23:53:22 +0200

    Added test comparing the LDOS generated by the Diagaonlizer and the ChebyshevExpander.

--- Test/include/TBTK/Test/PropertyExtractor/ChebyshevExpander.h
+++ Test/include/TBTK/Test/PropertyExtractor/ChebyshevExpander.h
@@ -1,5 +1,8 @@
 #include "TBTK/PropertyExtractor/ChebyshevExpander.h"
+#include "TBTK/PropertyExtractor/Diagonalizer.h"
+#include "TBTK/Smooth.h"
 #include "TBTK/Solver/ChebyshevExpander.h"
+#include "TBTK/Solver/Diagonalizer.h"
 
 #include "gtest/gtest.h"
 
@@ -13,7 +16,7 @@ const double CHEMICAL_POTENTIAL = 1;
 //TODO
 //Add tests for most functions.
 
-TEST(ChebyshevExpander, calculateLDOS){
+TEST(ChebyshevExpander, calculateLDOS0){
 	const unsigned int SIZE = 10;
 	Model model;
 	for(unsigned int n = 0; n < SIZE; n++)
@@ -39,6 +42,60 @@ TEST(ChebyshevExpander, calculateLDOS){
 			integratedLDOS += ldos({0}, n)*dE;
 		EXPECT_NEAR(integratedLDOS, 1, 0.001);
 	}
+}
+
+TEST(ChebyshevExpander, calculateLDOS1){
+	const unsigned int SIZE = 10;
+	Model model;
+	for(unsigned int n = 0; n < SIZE; n++)
+		model << HoppingAmplitude(-1, {n}, {(n+1)%SIZE}) + HC;
+	model.construct();
+
+	Solver::ChebyshevExpander solver;
+	solver.setModel(model);
+	const double SCALE_FACTOR = 10;
+	solver.setScaleFactor(SCALE_FACTOR);
+
+	Solver::Diagonalizer solverReference;
+	solverReference.setModel(model);
+	solverReference.run();
+
+	ChebyshevExpander propertyExtractor(solver);
+	Diagonalizer propertyExtractorReference;
+	propertyExtractorReference.setSolver(solverReference);
+
+	const double LOWER_BOUND = -SCALE_FACTOR*0.9;
+	const double UPPER_BOUND = SCALE_FACTOR*0.9;
+	const unsigned int RESOLUTION = 1000;
+	propertyExtractor.setEnergyWindow(
+		LOWER_BOUND,
+		UPPER_BOUND,
+		RESOLUTION
+	);
+	propertyExtractorReference.setEnergyWindow(
+		LOWER_BOUND,
+		UPPER_BOUND,
+		RESOLUTION
+	);
+	Property::LDOS ldos = propertyExtractor.calculateLDOS({{0}});
+	Property::LDOS ldosReference
+		= propertyExtractorReference.calculateLDOS({{0}});
+
+	//Smooth the result to even out differences. The Diagonalizer
+	//genereates very unsmooth data, while the ChebyshevExpander generates
+	//smoother data. Putting them both through a smoother evens out the
+	//differences.
+	const double SMOOTHING_SIGMA = 0.2;
+	const unsigned int SMOOTHING_WINDOW = 101;
+	ldos = Smooth::gaussian(ldos, SMOOTHING_SIGMA, SMOOTHING_WINDOW);
+	ldosReference = Smooth::gaussian(
+		ldosReference,
+		SMOOTHING_SIGMA,
+		SMOOTHING_WINDOW
+	);
+
+	for(unsigned int n = 0; n < ldos.getResolution(); n++)
+		EXPECT_NEAR(ldos({0}, n), ldosReference({0}, n), 0.02);
 }
 
 };	//End of namespace PropertyExtractor
